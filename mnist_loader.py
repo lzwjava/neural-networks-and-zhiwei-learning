5ec8ade9c2356d179547312b274a17110404a007
Zhiwei Li <lzwjava@gmail.com>
2023-06-20 21:47:18 +0800

    add

--- mnist_loader.py
+++ mnist_loader.py
@@ -3,11 +3,17 @@ import gzip
 
 import numpy as np
 
+
 def load_data():
     f = gzip.open('mnist.pkl.gz', 'rb')
-    training_data, validation_data, test_data = pickle.load(f, encoding='latin1')
+    training_data, validation_data, test_data = pickle.load(
+        f, encoding='latin1')
     f.close()
-    return (training_data validation_data, test_data)
+    return (training_data, validation_data, test_data)
 
-load_data()
 
+td, vd, td1 = load_data()
+
+print(td)
+print(vd)
+print(td1)
