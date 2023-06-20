d5c58ebb5430476e41ec18e81050bec80ed6697d
Zhiwei Li <lzwjava@gmail.com>
2023-06-20 22:53:00 +0800

    add

--- mnist_loader_1.py
+++ mnist_loader_1.py
@@ -18,7 +18,12 @@ def load_data():
 def load_data_wrapper():
     tr_d, va_d, te_d = load_data()
     training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
+    print(len(training_inputs))
+
     training_results = [vectorized_result(y) for y in tr_d[1]]
+    print(len(training_results))
+    print(training_results[0])
+
     training_data = zip(training_inputs, training_results)
     validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
     validation_data = zip(validation_inputs, va_d[1])
