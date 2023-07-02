ae54e35e990c4eb371e35e59a9912932050151ab
Zhiwei Li <lzwjava@gmail.com>
2023-07-03 01:18:15 +0800

    add

---
+++ network_1.py
@@ -0,0 +1,28 @@
+import random
+
+import numpy as np
+
+
+class Network(object):
+
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta,
+            test_data=None):
+        training_data = list(training_data)
+        n = len(training_data)
+
+        if test_data:
+            test_data = list(test_data)
+            n_test = len(test_data)
+
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in range(0, n, mini_batch_size)]
