102ba9c315110ccf4947c1e8fc5a6abeac8ea8e8
Zhiwei Li <lzwjava@gmail.com>
2023-07-02 23:40:59 +0800

    add

--- mnist_loader_4.py
+++ mnist_loader_4.py
@@ -3,9 +3,6 @@ import gzip
 
 import numpy as np
 
-import matplotlib
-import matplotlib.pyplot as plt
-
 
 def load_data():
     f = gzip.open('mnist.pkl.gz', 'rb')
@@ -23,26 +20,9 @@ def print_shape(array):
 def load_data_wrapper():
     tr_d, va_d, te_d = load_data()
 
-    # print(tr_d[0])
-    # print_shape(tr_d[0])
-    # print(tr_d[0][1])
-    # print_shape(tr_d[0][1])
-    # print_shape(training_inputs)
-
-    # print(training_inputs[0])
-    # print_shape(training_inputs[0])
-    # print(training_inputs[0])
-    print(tr_d[1])
-    print_shape(tr_d[1])
-
     training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
     training_results = [vectorized_result(y) for y in tr_d[1]]
     training_data = zip(training_inputs, training_results)
-
-    print(list(training_data)[0:1])
-
-    print_shape(training_inputs[0])
-    print_shape(training_results[0])
 
     validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
     validation_data = zip(validation_inputs, va_d[1])
@@ -56,6 +36,3 @@ def vectorized_result(j):
     e = np.zeros((10, 1))
     e[j] = 1.0
     return e
-
-
-td, vd, td1 = load_data_wrapper()
