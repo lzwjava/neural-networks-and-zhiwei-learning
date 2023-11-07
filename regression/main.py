968c848bd7021bebf5feea7c7550d62cfc8641cb
lzwjava <lzwjava@gmail.com>
2023-11-07 17:14:04 +0800

    add

--- regression/main.py
+++ regression/main.py
@@ -1,4 +1,9 @@
 import torch
+from itertools import count
+
+POLY_DEGREE = 4
+W_target = torch.randn(POLY_DEGREE, 1) * 5
+b_target = torch.randn(1) * 5
 
 
 def poly_desc(W, b):
@@ -9,15 +14,37 @@ def poly_desc(W, b):
     return result
 
 
+def make_features(x):
+    x = x.unsqueeze(1)
+    return torch.cat([x ** i for i in range(1, POLY_DEGREE + 1)], 1)
+
+
+def f(x):
+    return x.mm(W_target) + b_target.item()
+
+
+def get_batch(batch_size=32):
+    random = torch.randn(32)
+    x = make_features(random)
+    y = f(x)
+    return x, y
+
+
+def train():
+    fc = torch.nn.Linear(W_target.size(0), 1)
+    for batch_idx in count(1):
+        batch_x, batch_y = get_batch()
+
+
 def main():
-    POLY_DEGREE = 4
-    W_target = torch.randn(POLY_DEGREE, 1) * 5
-    b_target = torch.randn(1) * 5
     print(W_target)
     print(b_target)
 
     desc = poly_desc(W_target.view(-1), b_target)
     print(desc)
+
+    batch = get_batch()
+    print(batch)
 
 
 if __name__ == '__main__':
