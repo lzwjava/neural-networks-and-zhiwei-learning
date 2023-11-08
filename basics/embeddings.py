dc4eea2cc884943759969f1924cea46ddd8a8475
lzwjava <lzwjava@gmail.com>
2023-11-09 00:35:17 +0800

    add

--- basics/embeddings.py
+++ basics/embeddings.py
@@ -1,5 +1,12 @@
 import torch.nn as nn
+import torch
 
 word_to_ix = {"hello": 0, "world": 1}
 embeds = nn.Embedding(2, 5)
 print(embeds.weight)
+
+lookup_tensor = torch.tensor([word_to_ix['hello']], dtype=torch.long)
+hello_embed = embeds(lookup_tensor)
+print(hello_embed)
+
+print(embeds(torch.tensor(1)))
