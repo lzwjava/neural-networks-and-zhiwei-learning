3fcb0542fd0fe0118f505e3704857e24dd25c36f
lzwjava <lzwjava@gmail.com>
2023-11-08 20:44:34 +0800

    add

--- polynomial/polynomial_module.py
+++ polynomial/polynomial_module.py
@@ -1,5 +1,9 @@
+import math
+
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
 
 class Polynomial(nn.Module):
@@ -12,9 +16,33 @@ class Polynomial(nn.Module):
         return x
 
 
+def plot(x, y):
+    print(x)
+    print(y)
+
+    x = x.numpy()
+    y = y.numpy()
+
+    plt.plot(x, y)
+    plt.show()
+
+
 def main():
     model = Polynomial()
     loss_fn = nn.MSELoss()
+
+    step = 0
+    lr = 1e-5
+
+    optimizier = optim.SGD(model.parameters(), lr)
+
+    x = torch.linspace(-math.pi, math.pi, 2000)
+    y = torch.sin(x)
+
+    # plot(x, y)
+
+    while True:
+        pass
 
 
 if __name__ == '__main__':
