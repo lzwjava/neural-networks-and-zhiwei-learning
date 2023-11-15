31f282b5efc7d4fb02deb81105728f73d6dee2d4
lzwjava <lzwjava@gmail.com>
2023-11-16 02:03:08 +0800

    add

--- basics/sequence_model.py
+++ basics/sequence_model.py
@@ -1,5 +1,9 @@
+from typing import Dict, List
+
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
 torch.manual_seed(1)
 
@@ -23,8 +27,9 @@ for i in inputs:
     out, hidden = lstm(i.view(1, 1, -1), hidden)
 
 
-def prepare_sequence(seq, to_ix):
-    pass
+def prepare_sequence(seq: List[str], to_ix: Dict[str, int]) -> torch.Tensor:
+    idxs = [to_ix[w] for w in seq]
+    return torch.tensor(idxs, dtype=torch.long)
 
 
 training_data = [
@@ -57,10 +62,49 @@ class LSTMTagger(nn.Module):
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
         self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
 
-    def forward(self, sentence):
+    def forward(self, sentence: torch.Tensor):
+        # print(f'{sentence.size()=}')
+        # print(f'{sentence=}')
         embeds = self.word_embeddings(sentence)
-        return embeds
+        batch = len(sentence)
+        lstm_out, _ = self.lstm(embeds.view(batch, 1, -1))
+        out = self.hidden2tag(lstm_out.view(batch, -1))
+        tag_scores = F.log_softmax(out, dim=1)
+        return tag_scores
 
 
 model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_idx), len(tag_to_idx))
 loss_fn = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+
+def evaluate():
+    with torch.no_grad():
+        # print(f'{training_data[0][0]=}')
+        idxs = prepare_sequence(training_data[0][0], word_to_idx)
+        res = model(idxs)
+        print(f'{res=}')
+
+
+evaluate()
+
+
+def train():
+    for epoch in range(300):
+        for sentence, tags in training_data:
+            # print(f'{sentence=}')
+            # print(f'{tags=}')
+            model.zero_grad()
+
+            sentence_in = prepare_sequence(sentence, word_to_idx)
+            targets = prepare_sequence(tags, tag_to_idx)
+
+            tag_scores = model(sentence_in)
+            loss = loss_fn(tag_scores, targets)
+            loss.backward()
+            optimizer.step()
+
+
+train()
+
+evaluate()
