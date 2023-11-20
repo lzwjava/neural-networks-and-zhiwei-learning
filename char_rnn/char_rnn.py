16bcf2b7f70bc942142d87ccb64ef08ff2706d2c
lzwjava <lzwjava@gmail.com>
2023-11-20 20:15:06 +0800

    add

--- char_rnn/char_rnn.py
+++ char_rnn/char_rnn.py
@@ -1,5 +1,11 @@
+import os
+
+
 def main():
-    print('main')
+    dir_path = 'data/names'
+    for name in os.listdir(dir_path):
+        with open(os.path.join(dir_path, name)) as f:
+            print(f.read())
 
 
 if __name__ == '__main__':
