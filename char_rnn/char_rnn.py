4edb5059078b0ac8ca862766871ae5245838df4f
lzwjava <lzwjava@gmail.com>
2023-11-20 20:22:52 +0800

    add

--- char_rnn/char_rnn.py
+++ char_rnn/char_rnn.py
@@ -4,8 +4,11 @@ import os
 def main():
     dir_path = 'data/names'
     for name in os.listdir(dir_path):
+        print(name)
         with open(os.path.join(dir_path, name)) as f:
-            print(f.read())
+            # print('name')
+            pass
+            # print(f.read())
 
 
 if __name__ == '__main__':
