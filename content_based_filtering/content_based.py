eb7e06cf75663311fc7e501c6eff8c19760424f5
Zhiwei Li <lzwjava@gmail.com>
2023-11-28 01:33:58 +0800

    add

--- content_based_filtering/content_based.py
+++ content_based_filtering/content_based.py
@@ -1,11 +1,10 @@
-import numpy as np
 import numpy.ma as ma
 import pandas as pd
 import tensorflow as tf
-from tensorflow import keras
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
-import tabulate
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from tensorflow import keras
+
 from recsysNN_utils import *
 
 pd.set_option("display.precision", 1)
