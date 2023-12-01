c1b48c3f103e4a11861e662a1625ba8865f4e037
lzwjava <lzwjava@gmail.com>
2023-12-02 01:04:44 +0800

    add

---
+++ gradient_checking/gradient_checking.py
@@ -0,0 +1,14 @@
+import numpy as np
+from testCases import *
+from public_tests import *
+from gc_utils import sigmoid, relu, dictionary_to_vector, vector_to_dictionary, gradients_to_vector
+
+
+def forward_propagation(x, theta):
+    return J
+
+
+x, theta = 2, 4
+J = forward_propagation(x, theta)
+print("J = " + str(J))
+forward_propagation_test(forward_propagation)
