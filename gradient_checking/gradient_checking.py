184348417b89787b03a2180b2bea8064cc165a1b
lzwjava <lzwjava@gmail.com>
2023-12-02 01:05:14 +0800

    add

--- gradient_checking/gradient_checking.py
+++ gradient_checking/gradient_checking.py
@@ -1,7 +1,4 @@
-import numpy as np
-from testCases import *
 from public_tests import *
-from gc_utils import sigmoid, relu, dictionary_to_vector, vector_to_dictionary, gradients_to_vector
 
 
 def forward_propagation(x, theta):
