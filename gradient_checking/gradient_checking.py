9a4cf85dd52d048951640378bdfe298b32fe3e40
lzwjava <lzwjava@gmail.com>
2023-12-02 08:03:27 +0800

    add

--- gradient_checking/gradient_checking.py
+++ gradient_checking/gradient_checking.py
@@ -88,13 +88,13 @@ def backward_propagation_n(X, Y, cache):
 
     dA2 = np.dot(W3.T, dZ3)
     dZ2 = np.multiply(dA2, np.int64(A2 > 0))
-    dW2 = 1. / m * np.dot(dZ2, A1.T) * 2
+    dW2 = 1. / m * np.dot(dZ2, A1.T)
     db2 = 1. / m * np.sum(dZ2, axis=1, keepdims=True)
 
     dA1 = np.dot(W2.T, dZ2)
     dZ1 = np.multiply(dA1, np.int64(A1 > 0))
     dW1 = 1. / m * np.dot(dZ1, X.T)
-    db1 = 4. / m * np.sum(dZ1, axis=1, keepdims=True)
+    db1 = 1. / m * np.sum(dZ1, axis=1, keepdims=True)
 
     gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,
                  "dA2": dA2, "dZ2": dZ2, "dW2": dW2, "db2": db2,
@@ -114,11 +114,11 @@ def gradient_check_n(parameters, gradients, X, Y, epsilon=1e-7, print_msg=False)
 
     for i in range(num_parameters):
         theta_plus = np.copy(parameters_values)
-        theta_plus[i] = theta_plus[i] + epsilon
+        theta_plus[i][0] = theta_plus[i][0] + epsilon
         J_plus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(theta_plus))
 
         theta_minus = np.copy(parameters_values)
-        theta_minus[i] = theta_minus[i] - epsilon
+        theta_minus[i][0] = theta_minus[i][0] - epsilon
         J_minus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(theta_minus))
 
         gradapprox[i] = (J_plus[i] - J_minus[i]) / (2 * epsilon)
