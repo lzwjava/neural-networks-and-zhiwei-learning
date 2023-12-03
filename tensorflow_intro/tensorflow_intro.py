13493ca12a90226e58e7ce1788d6b2dd8bb71ea1
lzwjava <lzwjava@gmail.com>
2023-12-04 04:56:13 +0800

    add

--- tensorflow_intro/tensorflow_intro.py
+++ tensorflow_intro/tensorflow_intro.py
@@ -1,10 +1,9 @@
 import h5py
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 from tensorflow.python.framework.ops import EagerTensor
 from tensorflow.python.ops.resource_variable_ops import ResourceVariable
-import time
 
 train_dataset = h5py.File('datasets/train_signs.h5', "r")
 test_dataset = h5py.File('datasets/test_signs.h5', "r")
@@ -127,30 +126,15 @@ new_y_train = y_train.map(one_hot_matrix)
 print(next(iter(new_y_test)))
 
 
-# GRADED FUNCTION: initialize_parameters
-
 def initialize_parameters():
-    """
-    Initializes parameters to build a neural network with TensorFlow. The shapes are:
-                        W1 : [25, 12288]
-                        b1 : [25, 1]
-                        W2 : [12, 25]
-                        b2 : [12, 1]
-                        W3 : [6, 12]
-                        b3 : [6, 1]
+    initializer = tf.keras.initializers.GlorotNormal(seed=1)
 
-    Returns:
-    parameters -- a dictionary of tensors containing W1, b1, W2, b2, W3, b3
-    """
-
-    # initializer = tf.keras.initializers.GlorotNormal(seed=1)
-
-    W1 = tf.zeros((25, 12288))
-    b1 = tf.zeros((25, 1))
-    W2 = tf.zeros((12, 25))
-    b2 = tf.zeros((12, 1))
-    W3 = tf.zeros((6, 12))
-    b3 = tf.zeros((6, 1))
+    W1 = tf.Variable(initializer(shape=(25, 12288)))
+    b1 = tf.Variable(initializer(shape=(25, 1)))
+    W2 = tf.Variable(initializer(shape=(12, 25)))
+    b2 = tf.Variable(initializer(shape=(12, 1)))
+    W3 = tf.Variable(initializer(shape=(6, 12)))
+    b3 = tf.Variable(initializer(shape=(6, 1)))
 
     parameters = {"W1": W1,
                   "b1": b1,
@@ -184,3 +168,86 @@ def initialize_parameters_test(target):
 
 
 initialize_parameters_test(initialize_parameters)
+
+parameters = initialize_parameters()
+
+
+def forward_propagation(X, parameters):
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+
+    Z1 = tf.add(tf.matmul(W1, X), b1)
+    A1 = tf.nn.relu(Z1)
+    Z2 = tf.add(tf.matmul(W2, A1), b2)
+    A2 = tf.nn.relu(Z2)
+    Z3 = tf.add(tf.matmul(W3, A2), b3)
+
+    return Z3
+
+
+def forward_propagation_test(target, examples):
+    minibatches = examples.batch(2)
+    parametersk = initialize_parameters()
+    W1 = parametersk['W1']
+    b1 = parametersk['b1']
+    W2 = parametersk['W2']
+    b2 = parametersk['b2']
+    W3 = parametersk['W3']
+    b3 = parametersk['b3']
+    index = 0
+    minibatch = list(minibatches)[0]
+    with tf.GradientTape() as tape:
+        forward_pass = target(tf.transpose(minibatch), parametersk)
+        print(forward_pass)
+        fake_cost = tf.reduce_mean(forward_pass - np.ones((6, 2)))
+
+        assert type(forward_pass) == EagerTensor, "Your output is not a tensor"
+        assert forward_pass.shape == (6, 2), "Last layer must use W3 and b3"
+        assert np.allclose(forward_pass,
+                           [[-0.13430887, 0.14086473],
+                            [0.21588647, -0.02582335],
+                            [0.7059658, 0.6484556],
+                            [-1.1260961, -0.9329492],
+                            [-0.20181894, -0.3382722],
+                            [0.9558965, 0.94167566]]), "Output does not match"
+    index = index + 1
+    trainable_variables = [W1, b1, W2, b2, W3, b3]
+    grads = tape.gradient(fake_cost, trainable_variables)
+    assert not (None in grads), "Wrong gradients. It could be due to the use of tf.Variable whithin forward_propagation"
+    print("\033[92mAll test passed")
+
+
+forward_propagation_test(forward_propagation, new_train)
+
+
+def compute_total_loss(logits, labels):
+    total_loss = 0
+
+    return total_loss
+
+
+def compute_total_loss_test(target, Y):
+    pred = tf.constant([[2.4048107, 5.0334096],
+                        [-0.7921977, -4.1523376],
+                        [0.9447198, -0.46802214],
+                        [1.158121, 3.9810789],
+                        [4.768706, 2.3220146],
+                        [6.1481323, 3.909829]])
+    minibatches = Y.batch(2)
+    for minibatch in minibatches:
+        result = target(pred, tf.transpose(minibatch))
+        break
+
+    print(result)
+    assert (type(result) == EagerTensor), "Use the TensorFlow API"
+    assert (np.abs(result - (
+            0.50722074 + 1.1133534) / 2.0) < 1e-7), "Test does not match. Did you get the reduce sum of your loss functions?"
+
+    print("\033[92mAll test passed")
+
+
+compute_total_loss_test(compute_total_loss, new_y_train)
