b81f6135eb15dda257fc37c2e9e6a788714b88a1
lzwjava <lzwjava@gmail.com>
2023-12-03 21:06:48 +0800

    add

---
+++ optimization_methods/optimization.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+import math
+import sklearn
+import sklearn.datasets
+
+from opt_utils_v1a import load_params_and_grads, initialize_parameters, forward_propagation, backward_propagation
+from opt_utils_v1a import compute_cost, predict, predict_dec, plot_decision_boundary, load_dataset
+from copy import deepcopy
+from testCases import *
+from public_tests import *
+
+plt.rcParams['figure.figsize'] = (7.0, 4.0)  
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+
+
+def update_parameters_with_gd(parameters, grads, learning_rate):
+    L = len(parameters) // 2  
+
+    
+    for l in range(1, L + 1):
+        pass
+    
+    
+    
+    
+
+    
+    return parameters
+
+
+parameters, grads, learning_rate = update_parameters_with_gd_test_case()
+learning_rate = 0.01
+parameters = update_parameters_with_gd(parameters, grads, learning_rate)
+
+print("W1 =\n" + str(parameters["W1"]))
+print("b1 =\n" + str(parameters["b1"]))
+print("W2 =\n" + str(parameters["W2"]))
+print("b2 =\n" + str(parameters["b2"]))
+
+update_parameters_with_gd_test(update_parameters_with_gd)
