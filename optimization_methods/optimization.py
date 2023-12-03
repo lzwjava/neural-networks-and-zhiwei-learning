5b57edf087f13a6c898f02978443351ea4aceeac
lzwjava <lzwjava@gmail.com>
2023-12-03 22:35:02 +0800

    add

--- optimization_methods/optimization.py
+++ optimization_methods/optimization.py
@@ -42,54 +42,29 @@ print("b2 =\n" + str(parameters["b2"]))
 update_parameters_with_gd_test(update_parameters_with_gd)
 
 
-# GRADED FUNCTION: random_mini_batches
-
 def random_mini_batches(X, Y, mini_batch_size=64, seed=0):
-    """
-    Creates a list of random minibatches from (X, Y)
-
-    Arguments:
-    X -- input data, of shape (input size, number of examples)
-    Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
-    mini_batch_size -- size of the mini-batches, integer
-
-    Returns:
-    mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
-    """
-
-    np.random.seed(seed)  # To make your "random" minibatches the same as ours
-    m = X.shape[1]  # number of training examples
+    np.random.seed(seed)
+    m = X.shape[1]
     mini_batches = []
 
-    # Step 1: Shuffle (X, Y)
     permutation = list(np.random.permutation(m))
     shuffled_X = X[:, permutation]
     shuffled_Y = Y[:, permutation].reshape((1, m))
 
     inc = mini_batch_size
 
-    # Step 2 - Partition (shuffled_X, shuffled_Y).
-    # Cases with a complete mini batch size only i.e each of 64 examples.
     num_complete_minibatches = math.floor(
-        m / mini_batch_size)  # number of mini batches of size mini_batch_size in your partitionning
+        m / mini_batch_size)
     for k in range(0, num_complete_minibatches):
-        # (approx. 2 lines)
-        mini_batch_X = 0
-        mini_batch_Y = 0
-        # YOUR CODE STARTS HERE
+        mini_batch_X = shuffled_X[:, k * mini_batch_size: (k + 1) * mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k * mini_batch_size: (k + 1) * mini_batch_size]
 
-        # YOUR CODE ENDS HERE
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
 
-    # For handling the end case (last mini-batch < mini_batch_size i.e less than 64)
     if m % mini_batch_size != 0:
-        # (approx. 2 lines)
-        mini_batch_X = 0
-        mini_batch_Y = 0
-        # YOUR CODE STARTS HERE
-
-        # YOUR CODE ENDS HERE
+        mini_batch_X = shuffled_X[:, (k + 1) * mini_batch_size:]
+        mini_batch_Y = shuffled_Y[:, (k + 1) * mini_batch_size:]
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
 
