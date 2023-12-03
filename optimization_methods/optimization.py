cd417472f754835021a6646fc02c4826bbda5659
lzwjava <lzwjava@gmail.com>
2023-12-04 02:11:25 +0800

    add

--- optimization_methods/optimization.py
+++ optimization_methods/optimization.py
@@ -193,39 +193,12 @@ print("s[\"db2\"] = \n" + str(s["db2"]))
 initialize_adam_test(initialize_adam)
 
 
-# GRADED FUNCTION: update_parameters_with_adam
-
 def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate=0.01,
                                 beta1=0.9, beta2=0.999, epsilon=1e-8):
-    """
-    Update parameters using Adam
+    L = len(parameters) // 2
+    v_corrected = {}
+    s_corrected = {}
 
-    Arguments:
-    parameters -- python dictionary containing your parameters:
-                    parameters['W' + str(l)] = Wl
-                    parameters['b' + str(l)] = bl
-    grads -- python dictionary containing your gradients for each parameters:
-                    grads['dW' + str(l)] = dWl
-                    grads['db' + str(l)] = dbl
-    v -- Adam variable, moving average of the first gradient, python dictionary
-    s -- Adam variable, moving average of the squared gradient, python dictionary
-    t -- Adam variable, counts the number of taken steps
-    learning_rate -- the learning rate, scalar.
-    beta1 -- Exponential decay hyperparameter for the first moment estimates
-    beta2 -- Exponential decay hyperparameter for the second moment estimates
-    epsilon -- hyperparameter preventing division by zero in Adam updates
-
-    Returns:
-    parameters -- python dictionary containing your updated parameters
-    v -- Adam variable, moving average of the first gradient, python dictionary
-    s -- Adam variable, moving average of the squared gradient, python dictionary
-    """
-
-    L = len(parameters) // 2  # number of layers in the neural networks
-    v_corrected = {}  # Initializing first moment estimate, python dictionary
-    s_corrected = {}  # Initializing second moment estimate, python dictionary
-
-    # Perform Adam update on all parameters
     for l in range(1, L + 1):
         dWl = 'dW' + str(l)
         dbl = 'db' + str(l)
@@ -267,84 +240,52 @@ train_X, train_Y = load_dataset()
 
 def model(X, Y, layers_dims, optimizer, learning_rate=0.0007, mini_batch_size=64, beta=0.9,
           beta1=0.9, beta2=0.999, epsilon=1e-8, num_epochs=5000, print_cost=True):
-    """
-    3-layer neural network model which can be run in different optimizer modes.
+    L = len(layers_dims)
+    costs = []
+    t = 0
+    seed = 10
+    m = X.shape[1]
 
-    Arguments:
-    X -- input data, of shape (2, number of examples)
-    Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
-    optimizer -- the optimizer to be passed, gradient descent, momentum or adam
-    layers_dims -- python list, containing the size of each layer
-    learning_rate -- the learning rate, scalar.
-    mini_batch_size -- the size of a mini batch
-    beta -- Momentum hyperparameter
-    beta1 -- Exponential decay hyperparameter for the past gradients estimates
-    beta2 -- Exponential decay hyperparameter for the past squared gradients estimates
-    epsilon -- hyperparameter preventing division by zero in Adam updates
-    num_epochs -- number of epochs
-    print_cost -- True to print the cost every 1000 epochs
-
-    Returns:
-    parameters -- python dictionary containing your updated parameters
-    """
-
-    L = len(layers_dims)  # number of layers in the neural networks
-    costs = []  # to keep track of the cost
-    t = 0  # initializing the counter required for Adam update
-    seed = 10  # For grading purposes, so that your "random" minibatches are the same as ours
-    m = X.shape[1]  # number of training examples
-
-    # Initialize parameters
     parameters = initialize_parameters(layers_dims)
 
-    # Initialize the optimizer
     if optimizer == "gd":
-        pass  # no initialization required for gradient descent
+        pass
     elif optimizer == "momentum":
         v = initialize_velocity(parameters)
     elif optimizer == "adam":
         v, s = initialize_adam(parameters)
 
-    # Optimization loop
     for i in range(num_epochs):
 
-        # Define the random minibatches. We increment the seed to reshuffle differently the dataset after each epoch
         seed = seed + 1
         minibatches = random_mini_batches(X, Y, mini_batch_size, seed)
         cost_total = 0
 
         for minibatch in minibatches:
 
-            # Select a minibatch
             (minibatch_X, minibatch_Y) = minibatch
 
-            # Forward propagation
             a3, caches = forward_propagation(minibatch_X, parameters)
 
-            # Compute cost and add to the cost total
             cost_total += compute_cost(a3, minibatch_Y)
 
-            # Backward propagation
             grads = backward_propagation(minibatch_X, minibatch_Y, caches)
 
-            # Update parameters
             if optimizer == "gd":
                 parameters = update_parameters_with_gd(parameters, grads, learning_rate)
             elif optimizer == "momentum":
                 parameters, v = update_parameters_with_momentum(parameters, grads, v, beta, learning_rate)
             elif optimizer == "adam":
-                t = t + 1  # Adam counter
+                t = t + 1
                 parameters, v, s, _, _ = update_parameters_with_adam(parameters, grads, v, s,
                                                                      t, learning_rate, beta1, beta2, epsilon)
         cost_avg = cost_total / m
 
-        # Print the cost every 1000 epoch
         if print_cost and i % 1000 == 0:
             print("Cost after epoch %i: %f" % (i, cost_avg))
         if print_cost and i % 100 == 0:
             costs.append(cost_avg)
 
-    # plot the cost
     plt.plot(costs)
     plt.ylabel('cost')
     plt.xlabel('epochs (per 100)')
@@ -352,3 +293,175 @@ def model(X, Y, layers_dims, optimizer, learning_rate=0.0007, mini_batch_size=64
     plt.show()
 
     return parameters
+
+
+train_X, train_Y = load_dataset()
+
+
+def model(X, Y, layers_dims, optimizer, learning_rate=0.0007, mini_batch_size=64, beta=0.9,
+          beta1=0.9, beta2=0.999, epsilon=1e-8, num_epochs=5000, print_cost=True):
+    L = len(layers_dims)
+    costs = []
+    t = 0
+    seed = 10
+    m = X.shape[1]
+
+    parameters = initialize_parameters(layers_dims)
+
+    if optimizer == "gd":
+        pass
+    elif optimizer == "momentum":
+        v = initialize_velocity(parameters)
+    elif optimizer == "adam":
+        v, s = initialize_adam(parameters)
+
+    for i in range(num_epochs):
+
+        seed = seed + 1
+        minibatches = random_mini_batches(X, Y, mini_batch_size, seed)
+        cost_total = 0
+
+        for minibatch in minibatches:
+
+            (minibatch_X, minibatch_Y) = minibatch
+
+            a3, caches = forward_propagation(minibatch_X, parameters)
+
+            cost_total += compute_cost(a3, minibatch_Y)
+
+            grads = backward_propagation(minibatch_X, minibatch_Y, caches)
+
+            if optimizer == "gd":
+                parameters = update_parameters_with_gd(parameters, grads, learning_rate)
+            elif optimizer == "momentum":
+                parameters, v = update_parameters_with_momentum(parameters, grads, v, beta, learning_rate)
+            elif optimizer == "adam":
+                t = t + 1
+                parameters, v, s, _, _ = update_parameters_with_adam(parameters, grads, v, s,
+                                                                     t, learning_rate, beta1, beta2, epsilon)
+        cost_avg = cost_total / m
+
+        if print_cost and i % 1000 == 0:
+            print("Cost after epoch %i: %f" % (i, cost_avg))
+        if print_cost and i % 100 == 0:
+            costs.append(cost_avg)
+
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('epochs (per 100)')
+    plt.title("Learning rate = " + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+
+layers_dims = [train_X.shape[0], 5, 2, 1]
+parameters = model(train_X, train_Y, layers_dims, optimizer="gd")
+
+predictions = predict(train_X, train_Y, parameters)
+
+plt.title("Model with Gradient Descent optimization")
+axes = plt.gca()
+axes.set_xlim([-1.5, 2.5])
+axes.set_ylim([-1, 1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+layers_dims = [train_X.shape[0], 5, 2, 1]
+parameters = model(train_X, train_Y, layers_dims, beta=0.9, optimizer="momentum")
+
+predictions = predict(train_X, train_Y, parameters)
+
+plt.title("Model with Momentum optimization")
+axes = plt.gca()
+axes.set_xlim([-1.5, 2.5])
+axes.set_ylim([-1, 1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+layers_dims = [train_X.shape[0], 5, 2, 1]
+parameters = model(train_X, train_Y, layers_dims, optimizer="adam")
+
+predictions = predict(train_X, train_Y, parameters)
+
+plt.title("Model with Adam optimization")
+axes = plt.gca()
+axes.set_xlim([-1.5, 2.5])
+axes.set_ylim([-1, 1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+def model(X, Y, layers_dims, optimizer, learning_rate=0.0007, mini_batch_size=64, beta=0.9,
+          beta1=0.9, beta2=0.999, epsilon=1e-8, num_epochs=5000, print_cost=True, decay=None, decay_rate=1):
+    L = len(layers_dims)
+    costs = []
+    t = 0
+    seed = 10
+    m = X.shape[1]
+    lr_rates = []
+    learning_rate0 = learning_rate
+
+    parameters = initialize_parameters(layers_dims)
+
+    if optimizer == "gd":
+        pass
+    elif optimizer == "momentum":
+        v = initialize_velocity(parameters)
+    elif optimizer == "adam":
+        v, s = initialize_adam(parameters)
+
+    for i in range(num_epochs):
+
+        seed = seed + 1
+        minibatches = random_mini_batches(X, Y, mini_batch_size, seed)
+        cost_total = 0
+
+        for minibatch in minibatches:
+
+            (minibatch_X, minibatch_Y) = minibatch
+
+            a3, caches = forward_propagation(minibatch_X, parameters)
+
+            cost_total += compute_cost(a3, minibatch_Y)
+
+            grads = backward_propagation(minibatch_X, minibatch_Y, caches)
+
+            if optimizer == "gd":
+                parameters = update_parameters_with_gd(parameters, grads, learning_rate)
+            elif optimizer == "momentum":
+                parameters, v = update_parameters_with_momentum(parameters, grads, v, beta, learning_rate)
+            elif optimizer == "adam":
+                t = t + 1
+                parameters, v, s, _, _ = update_parameters_with_adam(parameters, grads, v, s,
+                                                                     t, learning_rate, beta1, beta2, epsilon)
+        cost_avg = cost_total / m
+        if decay:
+            learning_rate = decay(learning_rate0, i, decay_rate)
+
+        if print_cost and i % 1000 == 0:
+            print("Cost after epoch %i: %f" % (i, cost_avg))
+            if decay:
+                print("learning rate after epoch %i: %f" % (i, learning_rate))
+        if print_cost and i % 100 == 0:
+            costs.append(cost_avg)
+
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('epochs (per 100)')
+    plt.title("Learning rate = " + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+
+def update_lr(learning_rate0, epoch_num, decay_rate):
+    return learning_rate
+
+
+learning_rate = 0.5
+print("Original learning rate: ", learning_rate)
+epoch_num = 2
+decay_rate = 1
+learning_rate_2 = update_lr(learning_rate, epoch_num, decay_rate)
+
+print("Updated learning rate: ", learning_rate_2)
+
+update_lr_test(update_lr)
