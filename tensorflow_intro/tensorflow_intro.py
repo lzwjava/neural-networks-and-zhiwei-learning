af6d931b46d4dace2f6c6e8c14ed2ff39ca45d06
lzwjava <lzwjava@gmail.com>
2023-12-04 03:08:37 +0800

    add

---
+++ tensorflow_intro/tensorflow_intro.py
@@ -0,0 +1,22 @@
+import h5py
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.python.framework.ops import EagerTensor
+from tensorflow.python.ops.resource_variable_ops import ResourceVariable
+import time
+
+train_dataset = h5py.File('datasets/train_signs.h5', "r")
+test_dataset = h5py.File('datasets/test_signs.h5', "r")
+
+x_train = tf.data.Dataset.from_tensor_slices(train_dataset['train_set_x'])
+y_train = tf.data.Dataset.from_tensor_slices(train_dataset['train_set_y'])
+
+x_test = tf.data.Dataset.from_tensor_slices(test_dataset['test_set_x'])
+y_test = tf.data.Dataset.from_tensor_slices(test_dataset['test_set_y'])
+
+type(x_train)
+
+print(x_train.element_spec)
+
+print(next(iter(x_train)))
