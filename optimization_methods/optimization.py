673c0163f25640fe1be215b59498117de23e5cdc
lzwjava <lzwjava@gmail.com>
2023-12-03 21:54:41 +0800

    add

--- optimization_methods/optimization.py
+++ optimization_methods/optimization.py
@@ -20,7 +20,12 @@ def update_parameters_with_gd(parameters, grads, learning_rate):
     L = len(parameters) // 2
 
     for l in range(1, L + 1):
-        pass
+        wl = 'W' + str(l)
+        bl = 'b' + str(l)
+        dWl = 'dW' + str(l)
+        dbl = 'db' + str(l)
+        parameters[wl] = parameters[wl] - learning_rate * grads[dWl]
+        parameters[bl] = parameters[bl] - learning_rate * grads[dbl]
 
     return parameters
 
@@ -35,3 +40,86 @@ print("W2 =\n" + str(parameters["W2"]))
 print("b2 =\n" + str(parameters["b2"]))
 
 update_parameters_with_gd_test(update_parameters_with_gd)
+
+
+# GRADED FUNCTION: random_mini_batches
+
+def random_mini_batches(X, Y, mini_batch_size=64, seed=0):
+    """
+    Creates a list of random minibatches from (X, Y)
+
+    Arguments:
+    X -- input data, of shape (input size, number of examples)
+    Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
+    mini_batch_size -- size of the mini-batches, integer
+
+    Returns:
+    mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
+    """
+
+    np.random.seed(seed)  # To make your "random" minibatches the same as ours
+    m = X.shape[1]  # number of training examples
+    mini_batches = []
+
+    # Step 1: Shuffle (X, Y)
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[:, permutation].reshape((1, m))
+
+    inc = mini_batch_size
+
+    # Step 2 - Partition (shuffled_X, shuffled_Y).
+    # Cases with a complete mini batch size only i.e each of 64 examples.
+    num_complete_minibatches = math.floor(
+        m / mini_batch_size)  # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        # (approx. 2 lines)
+        mini_batch_X = 0
+        mini_batch_Y = 0
+        # YOUR CODE STARTS HERE
+
+        # YOUR CODE ENDS HERE
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    # For handling the end case (last mini-batch < mini_batch_size i.e less than 64)
+    if m % mini_batch_size != 0:
+        # (approx. 2 lines)
+        mini_batch_X = 0
+        mini_batch_Y = 0
+        # YOUR CODE STARTS HERE
+
+        # YOUR CODE ENDS HERE
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    return mini_batches
+
+
+np.random.seed(1)
+mini_batch_size = 64
+nx = 12288
+m = 148
+X = np.array([x for x in range(nx * m)]).reshape((m, nx)).T
+Y = np.random.randn(1, m) < 0.5
+
+mini_batches = random_mini_batches(X, Y, mini_batch_size)
+n_batches = len(mini_batches)
+
+assert n_batches == math.ceil(
+    m / mini_batch_size), f"Wrong number of mini batches. {n_batches} != {math.ceil(m / mini_batch_size)}"
+for k in range(n_batches - 1):
+    assert mini_batches[k][0].shape == (nx, mini_batch_size), f"Wrong shape in {k} mini batch for X"
+    assert mini_batches[k][1].shape == (1, mini_batch_size), f"Wrong shape in {k} mini batch for Y"
+    assert np.sum(np.sum(mini_batches[k][0] - mini_batches[k][0][0], axis=0)) == ((nx * (
+            nx - 1) / 2) * mini_batch_size), "Wrong values. It happens if the order of X rows(features) changes"
+if (m % mini_batch_size > 0):
+    assert mini_batches[n_batches - 1][0].shape == (nx,
+                                                    m % mini_batch_size), f"Wrong shape in the last minibatch. {mini_batches[n_batches - 1][0].shape} != {(nx, m % mini_batch_size)}"
+
+assert np.allclose(mini_batches[0][0][0][0:3],
+                   [294912, 86016, 454656]), "Wrong values. Check the indexes used to form the mini batches"
+assert np.allclose(mini_batches[-1][0][-1][0:3],
+                   [1425407, 1769471, 897023]), "Wrong values. Check the indexes used to form the mini batches"
+
+print("\033[92mAll tests passed!")
