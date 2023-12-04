96f5b9e56e4da3c47ffbf2a7d235cbddf0f9b5eb
lzwjava <lzwjava@gmail.com>
2023-12-05 06:12:14 +0800

    add

--- convolution_model/convolution.py
+++ convolution_model/convolution.py
@@ -241,3 +241,55 @@ assert np.isclose(np.mean(dW), 1.7269914), "Wrong values for dW"
 assert np.isclose(np.mean(db), 7.8392325), "Wrong values for db"
 
 print("\033[92m All tests passed.")
+
+
+def create_mask_from_window(x):
+    mask = (x == np.max(x))
+    return mask
+
+
+np.random.seed(1)
+x = np.random.randn(2, 3)
+mask = create_mask_from_window(x)
+print('x = ', x)
+print("mask = ", mask)
+
+x = np.array([[-1, 2, 3],
+              [2, -3, 2],
+              [1, 5, -2]])
+
+y = np.array([[False, False, False],
+              [False, False, False],
+              [False, True, False]])
+mask = create_mask_from_window(x)
+
+assert type(mask) == np.ndarray, "Output must be a np.ndarray"
+assert mask.shape == x.shape, "Input and output shapes must match"
+assert np.allclose(mask, y), "Wrong output. The True value must be at position (2, 1)"
+
+print("\033[92m All tests passed.")
+
+
+def distribute_value(dz, shape):
+    (n_H, n_W) = shape
+
+    average = dz / (n_H * n_W)
+
+    a = np.ones(shape) * average
+
+    return a
+
+
+a = distribute_value(2, (2, 2))
+print('distributed value =', a)
+
+assert type(a) == np.ndarray, "Output must be a np.ndarray"
+assert a.shape == (2, 2), f"Wrong shape {a.shape} != (2, 2)"
+assert np.sum(a) == 2, "Values must sum to 2"
+
+a = distribute_value(100, (10, 10))
+assert type(a) == np.ndarray, "Output must be a np.ndarray"
+assert a.shape == (10, 10), f"Wrong shape {a.shape} != (10, 10)"
+assert np.sum(a) == 100, "Values must sum to 100"
+
+print("\033[92m All tests passed.")
