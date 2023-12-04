b943f5366790fed46671c79ad3448928b540e53f
lzwjava <lzwjava@gmail.com>
2023-12-05 04:43:57 +0800

    add

--- convolution_model/convolution.py
+++ convolution_model/convolution.py
@@ -60,3 +60,92 @@ conv_single_step_test(conv_single_step)
 
 assert (type(Z) == np.float64), "You must cast the output to numpy float 64"
 assert np.isclose(Z, -6.999089450680221), "Wrong value"
+
+
+# GRADED FUNCTION: conv_forward
+
+def conv_forward(A_prev, W, b, hparameters):
+    """
+    Implements the forward propagation for a convolution function
+
+    Arguments:
+    A_prev -- output activations of the previous layer,
+        numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
+    b -- Biases, numpy array of shape (1, 1, 1, n_C)
+    hparameters -- python dictionary containing "stride" and "pad"
+
+    Returns:
+    Z -- conv output, numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache of values needed for the conv_backward() function
+    """
+
+    # Retrieve dimensions from A_prev's shape (≈1 line)
+    # (m, n_H_prev, n_W_prev, n_C_prev) = None
+
+    # Retrieve dimensions from W's shape (≈1 line)
+    # (f, f, n_C_prev, n_C) = None
+
+    # Retrieve information from "hparameters" (≈2 lines)
+    # stride = None
+    # pad = None
+
+    # Compute the dimensions of the CONV output volume using the formula given above.
+    # Hint: use int() to apply the 'floor' operation. (≈2 lines)
+    # n_H = None
+    # n_W = None
+
+    # Initialize the output volume Z with zeros. (≈1 line)
+    # Z = None
+
+    # Create A_prev_pad by padding A_prev
+    # A_prev_pad = None
+
+    # for i in range(None):               # loop over the batch of training examples
+    # a_prev_pad = None               # Select ith training example's padded activation
+    # for h in range(None):           # loop over vertical axis of the output volume
+    # Find the vertical start and end of the current "slice" (≈2 lines)
+    # vert_start = None
+    # vert_end = None
+
+    # for w in range(None):       # loop over horizontal axis of the output volume
+    # Find the horizontal start and end of the current "slice" (≈2 lines)
+    # horiz_start = None
+    # horiz_end = None
+
+    # for c in range(None):   # loop over channels (= #filters) of the output volume
+
+    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
+    # a_slice_prev = None
+
+    # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. (≈3 line)
+    # weights = None
+    # biases = None
+    # Z[i, h, w, c] = None
+    # YOUR CODE STARTS HERE
+
+    # YOUR CODE ENDS HERE
+
+    # Save information in "cache" for the backprop
+    cache = (A_prev, W, b, hparameters)
+
+    return Z, cache
+
+
+np.random.seed(1)
+A_prev = np.random.randn(2, 5, 7, 4)
+W = np.random.randn(3, 3, 4, 8)
+b = np.random.randn(1, 1, 1, 8)
+hparameters = {"pad": 1,
+               "stride": 2}
+
+Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
+z_mean = np.mean(Z)
+z_0_2_1 = Z[0, 2, 1]
+cache_0_1_2_3 = cache_conv[0][1][2][3]
+print("Z's mean =\n", z_mean)
+print("Z[0,2,1] =\n", z_0_2_1)
+print("cache_conv[0][1][2][3] =\n", cache_0_1_2_3)
+
+conv_forward_test_1(z_mean, z_0_2_1, cache_0_1_2_3)
+conv_forward_test_2(conv_forward)
