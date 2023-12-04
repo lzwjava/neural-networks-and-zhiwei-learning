3a49680396ca1bffe83f14b8d14bea62fd9412b0
lzwjava <lzwjava@gmail.com>
2023-12-05 06:05:40 +0800

    add

--- convolution_model/convolution.py
+++ convolution_model/convolution.py
@@ -127,7 +127,7 @@ def pool_forward(A_prev, hparameters, mode="max"):
                 horiz_start = w
                 horiz_end = w + f
                 for c in range(n_C):
-                    a_slice_prev = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, :]
+                    a_slice_prev = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
                     if mode == 'max':
                         A[i, h // stride, w // stride, c] = np.max(a_slice_prev)
                     elif mode == 'average':
@@ -171,3 +171,94 @@ print("A.shape = " + str(A.shape))
 print("A[1] =\n", A[1])
 
 pool_forward_test_2(pool_forward)
+
+
+def conv_backward(dZ, cache):
+
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+
+    
+    
+
+    
+    
+    
+    
+
+    
+    
+    
+
+    
+
+    
+    
+    
+
+    
+    
+    
+
+    
+    
+    
+    
+    
+
+    
+    
+
+    
+    
+    
+    
+
+    
+    
+    
+
+    
+
+    
+    assert (dA_prev.shape == (m, n_H_prev, n_W_prev, n_C_prev))
+
+    return dA_prev, dW, db
+
+
+
+
+np.random.seed(1)
+A_prev = np.random.randn(10, 4, 4, 3)
+W = np.random.randn(2, 2, 3, 8)
+b = np.random.randn(1, 1, 1, 8)
+hparameters = {"pad": 2,
+               "stride": 2}
+Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
+
+
+dA, dW, db = conv_backward(Z, cache_conv)
+
+print("dA_mean =", np.mean(dA))
+print("dW_mean =", np.mean(dW))
+print("db_mean =", np.mean(db))
+
+assert type(dA) == np.ndarray, "Output must be a np.ndarray"
+assert type(dW) == np.ndarray, "Output must be a np.ndarray"
+assert type(db) == np.ndarray, "Output must be a np.ndarray"
+assert dA.shape == (10, 4, 4, 3), f"Wrong shape for dA  {dA.shape} != (10, 4, 4, 3)"
+assert dW.shape == (2, 2, 3, 8), f"Wrong shape for dW {dW.shape} != (2, 2, 3, 8)"
+assert db.shape == (1, 1, 1, 8), f"Wrong shape for db {db.shape} != (1, 1, 1, 8)"
+assert np.isclose(np.mean(dA), 1.4524377), "Wrong values for dA"
+assert np.isclose(np.mean(dW), 1.7269914), "Wrong values for dW"
+assert np.isclose(np.mean(db), 7.8392325), "Wrong values for db"
+
+print("\033[92m All tests passed.")
