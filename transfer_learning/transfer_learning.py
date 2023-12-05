e6defaa5ee78f682d0acb7a63f90963ac032849c
lzwjava <lzwjava@gmail.com>
2023-12-05 23:25:28 +0800

    add

--- transfer_learning/transfer_learning.py
+++ transfer_learning/transfer_learning.py
@@ -40,9 +40,10 @@ train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
 
 
 def data_augmenter():
-    data_augmentation = None
-    data_augmentation.add(None)
-    data_augmentation.add(None)
+    data_augmentation = tf.keras.Sequential()
+
+    data_augmentation.add(RandomFlip('horizontal'))
+    data_augmentation.add(RandomRotation(0.2))
 
     return data_augmentation
 
@@ -56,3 +57,117 @@ assert augmenter.layers[1].factor == 0.2, "Rotation factor must be 0.2"
 assert len(augmenter.layers) == 2, "The model must have only 2 layers"
 
 print('\033[92mAll tests passed!')
+
+data_augmentation = data_augmenter()
+
+for image, _ in train_dataset.take(1):
+    plt.figure(figsize=(10, 10))
+    first_image = image[0]
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        augmented_image = data_augmentation(tf.expand_dims(first_image, 0))
+        plt.imshow(augmented_image[0] / 255)
+        plt.axis('off')
+
+preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+
+IMG_SHAPE = IMG_SIZE + (3,)
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                               include_top=True,
+                                               weights='imagenet')
+
+base_model.summary()
+
+nb_layers = len(base_model.layers)
+print(base_model.layers[nb_layers - 2].name)
+print(base_model.layers[nb_layers - 1].name)
+
+image_batch, label_batch = next(iter(train_dataset))
+feature_batch = base_model(image_batch)
+print(feature_batch.shape)
+
+base_model.trainable = False
+image_var = tf.Variable(preprocess_input(image_batch))
+pred = base_model(image_var)
+
+tf.keras.applications.mobilenet_v2.decode_predictions(pred.numpy(), top=2)
+
+
+def alpaca_model(image_shape=IMG_SIZE, data_augmentation=data_augmenter()):
+    input_shape = image_shape + (3,)
+
+    base_model = tf.keras.applications.MobileNetV2(input_shape=None,
+                                                   include_top=None,
+                                                   weights=None)
+
+    base_model.trainable = None
+
+    inputs = tf.keras.Input(shape=None)
+
+    x = None
+
+    x = preprocess_input(None)
+
+    x = base_model(None, training=None)
+
+    x = None()(x)
+
+    x = None(None)(x)
+
+    outputs = None
+
+    model = tf.keras.Model(inputs, outputs)
+
+    return model
+
+
+model2 = alpaca_model(IMG_SIZE, data_augmentation)
+
+from test_utils import summary, comparator
+
+alpaca_summary = [['InputLayer', [(None, 160, 160, 3)], 0],
+                  ['Sequential', (None, 160, 160, 3), 0],
+                  ['TensorFlowOpLayer', [(None, 160, 160, 3)], 0],
+                  ['TensorFlowOpLayer', [(None, 160, 160, 3)], 0],
+                  ['Functional', (None, 5, 5, 1280), 2257984],
+                  ['GlobalAveragePooling2D', (None, 1280), 0],
+                  ['Dropout', (None, 1280), 0, 0.2],
+                  ['Dense', (None, 1), 1281, 'linear']]
+
+comparator(summary(model2), alpaca_summary)
+
+for layer in summary(model2):
+    print(layer)
+
+base_learning_rate = 0.001
+model2.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+               metrics=['accuracy'])
+
+initial_epochs = 5
+history = model2.fit(train_dataset, validation_data=validation_dataset, epochs=initial_epochs)
+
+acc = [0.] + history.history['accuracy']
+val_acc = [0.] + history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()), 1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0, 1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
