9b0d4d06d13bde008e8d4ec5ea20e45abd42c4cc
lzwjava <lzwjava@gmail.com>
2023-12-06 07:59:51 +0800

    add

---
+++ image_segmentation/segmentation.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import concatenate
+
+from test_utils import summary, comparator
+
+import os
+import numpy as np  
+import pandas as pd  
+
+import imageio
+
+import matplotlib.pyplot as plt
+
+path = ''
+image_path = os.path.join(path, './data/CameraRGB/')
+mask_path = os.path.join(path, './data/CameraMask/')
+image_list_orig = os.listdir(image_path)
+image_list = [image_path + i for i in image_list_orig]
+mask_list = [mask_path + i for i in image_list_orig]
+
+N = 2
+img = imageio.imread(image_list[N])
+mask = imageio.imread(mask_list[N])
+
+
+fig, arr = plt.subplots(1, 2, figsize=(14, 10))
+arr[0].imshow(img)
+arr[0].set_title('Image')
+arr[1].imshow(mask[:, :, 0])
+arr[1].set_title('Segmentation')
+
+image_list_ds = tf.data.Dataset.list_files(image_list, shuffle=False)
+mask_list_ds = tf.data.Dataset.list_files(mask_list, shuffle=False)
+
+for path in zip(image_list_ds.take(3), mask_list_ds.take(3)):
+    print(path)
+
+image_filenames = tf.constant(image_list)
+masks_filenames = tf.constant(mask_list)
+
+dataset = tf.data.Dataset.from_tensor_slices((image_filenames, masks_filenames))
+
+for image, mask in dataset.take(1):
+    print(image)
+    print(mask)
+
+
+def process_path(image_path, mask_path):
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_png(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    mask = tf.io.read_file(mask_path)
+    mask = tf.image.decode_png(mask, channels=3)
+    mask = tf.math.reduce_max(mask, axis=-1, keepdims=True)
+    return img, mask
+
+
+def preprocess(image, mask):
+    input_image = tf.image.resize(image, (96, 128), method='nearest')
+    input_mask = tf.image.resize(mask, (96, 128), method='nearest')
+
+    return input_image, input_mask
+
+
+image_ds = dataset.map(process_path)
+processed_image_ds = image_ds.map(preprocess)
