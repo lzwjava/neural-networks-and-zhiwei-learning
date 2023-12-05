db7562d966244001f8c489819c0d59faa1473968
lzwjava <lzwjava@gmail.com>
2023-12-05 17:39:58 +0800

    add

---
+++ convolution_model_app/cnn_app.py
@@ -0,0 +1,36 @@
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imread
+import scipy
+from PIL import Image
+import pandas as pd
+import tensorflow as tf
+import tensorflow.keras.layers as tfl
+from tensorflow.python.framework import ops
+from cnn_utils import *
+from test_utils import summary, comparator
+
+np.random.seed(1)
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_happy_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig / 255.
+X_test = X_test_orig / 255.
+
+# Reshape
+Y_train = Y_train_orig.T
+Y_test = Y_test_orig.T
+
+print("number of training examples = " + str(X_train.shape[0]))
+print("number of test examples = " + str(X_test.shape[0]))
+print("X_train shape: " + str(X_train.shape))
+print("Y_train shape: " + str(Y_train.shape))
+print("X_test shape: " + str(X_test.shape))
+print("Y_test shape: " + str(Y_test.shape))
+
+index = 124
+plt.imshow(X_train_orig[index])  # display sample training image
+plt.show()
