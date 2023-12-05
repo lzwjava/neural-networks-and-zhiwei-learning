185699e8abfcf0565fc144c3ece003129e9ac143
lzwjava <lzwjava@gmail.com>
2023-12-05 17:45:37 +0800

    add

--- convolution_model_app/cnn_app.py
+++ convolution_model_app/cnn_app.py
@@ -16,11 +16,9 @@ np.random.seed(1)
 
 X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_happy_dataset()
 
-# Normalize image vectors
 X_train = X_train_orig / 255.
 X_test = X_test_orig / 255.
 
-# Reshape
 Y_train = Y_train_orig.T
 Y_test = Y_test_orig.T
 
@@ -32,5 +30,29 @@ print("X_test shape: " + str(X_test.shape))
 print("Y_test shape: " + str(Y_test.shape))
 
 index = 124
-plt.imshow(X_train_orig[index])  # display sample training image
+plt.imshow(X_train_orig[index])
 plt.show()
+
+
+def happyModel():
+    model = tf.keras.Sequential([
+
+    ])
+
+    return model
+
+
+happy_model = happyModel()
+
+for layer in summary(happy_model):
+    print(layer)
+
+output = [['ZeroPadding2D', (None, 70, 70, 3), 0, ((3, 3), (3, 3))],
+          ['Conv2D', (None, 64, 64, 32), 4736, 'valid', 'linear', 'GlorotUniform'],
+          ['BatchNormalization', (None, 64, 64, 32), 128],
+          ['ReLU', (None, 64, 64, 32), 0],
+          ['MaxPooling2D', (None, 32, 32, 32), 0, (2, 2), (2, 2), 'valid'],
+          ['Flatten', (None, 32768), 0],
+          ['Dense', (None, 1), 32769, 'sigmoid']]
+
+comparator(summary(happy_model), output)
