897060f39b491b067c5ff8638d2a915d6a73b7fd
lzwjava <lzwjava@gmail.com>
2023-12-05 18:42:18 +0800

    add

--- residual_networks/res_net.py
+++ residual_networks/res_net.py
@@ -18,3 +18,55 @@ import public_tests
 
 np.random.seed(1)
 tf.random.set_seed(2)
+
+
+def identity_block(X, f, filters, initializer=random_uniform):
+    F1, F2, F3 = filters
+
+    X_shortcut = X
+
+    X = Conv2D(filters=F1, kernel_size=1, strides=(1, 1), padding='valid', kernel_initializer=initializer(seed=0))(X)
+    X = BatchNormalization(axis=3)(X)
+    X = Activation('relu')(X)
+
+    X = None
+    X = None
+    X = None
+
+    X = None
+    X = None
+
+    X = None
+    X = None
+
+    return X
+
+
+tf.keras.backend.set_learning_phase(False)
+
+np.random.seed(1)
+tf.random.set_seed(2)
+X1 = np.ones((1, 4, 4, 3)) * -1
+X2 = np.ones((1, 4, 4, 3)) * 1
+X3 = np.ones((1, 4, 4, 3)) * 3
+
+X = np.concatenate((X1, X2, X3), axis=0).astype(np.float32)
+
+A3 = identity_block(X, f=2, filters=[4, 4, 3],
+                    initializer=lambda seed=0: constant(value=1))
+print('\033[1mWith training=False\033[0m\n')
+A3np = A3.numpy()
+print(np.around(A3.numpy()[:, (0, -1), :, :].mean(axis=3), 5))
+resume = A3np[:, (0, -1), :, :].mean(axis=3)
+print(resume[1, 1, 0])
+
+tf.keras.backend.set_learning_phase(True)
+
+print('\n\033[1mWith training=True\033[0m\n')
+np.random.seed(1)
+tf.random.set_seed(2)
+A4 = identity_block(X, f=2, filters=[3, 3, 3],
+                    initializer=lambda seed=0: constant(value=1))
+print(np.around(A4.numpy()[:, (0, -1), :, :].mean(axis=3), 5))
+
+public_tests.identity_block_test(identity_block)
