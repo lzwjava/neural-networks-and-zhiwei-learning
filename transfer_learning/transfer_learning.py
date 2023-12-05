770da201279d8da65c696cc621b4e93f1300be2c
lzwjava <lzwjava@gmail.com>
2023-12-05 22:58:24 +0800

    add

---
+++ transfer_learning/transfer_learning.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import tensorflow as tf
+import tensorflow.keras.layers as tfl
+
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+from tensorflow.keras.layers.experimental.preprocessing import RandomFlip, RandomRotation
+
+BATCH_SIZE = 32
+IMG_SIZE = (160, 160)
+directory = "dataset/"
+train_dataset = image_dataset_from_directory(directory,
+                                             shuffle=True,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE,
+                                             validation_split=0.2,
+                                             subset='training',
+                                             seed=42)
+validation_dataset = image_dataset_from_directory(directory,
+                                                  shuffle=True,
+                                                  batch_size=BATCH_SIZE,
+                                                  image_size=IMG_SIZE,
+                                                  validation_split=0.2,
+                                                  subset='validation',
+                                                  seed=42)
+
+class_names = train_dataset.class_names
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_dataset.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
