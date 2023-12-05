34f03559d8997845f34185904c3054775ae9d0a8
lzwjava <lzwjava@gmail.com>
2023-12-05 22:59:47 +0800

    add

--- transfer_learning/transfer_learning.py
+++ transfer_learning/transfer_learning.py
@@ -34,3 +34,25 @@ for images, labels in train_dataset.take(1):
         plt.imshow(images[i].numpy().astype("uint8"))
         plt.title(class_names[labels[i]])
         plt.axis("off")
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+
+
+def data_augmenter():
+    data_augmentation = None
+    data_augmentation.add(None)
+    data_augmentation.add(None)
+
+    return data_augmentation
+
+
+augmenter = data_augmenter()
+
+assert (augmenter.layers[0].name.startswith('random_flip')), "First layer must be RandomFlip"
+assert augmenter.layers[0].mode == 'horizontal', "RadomFlip parameter must be horizontal"
+assert (augmenter.layers[1].name.startswith('random_rotation')), "Second layer must be RandomRotation"
+assert augmenter.layers[1].factor == 0.2, "Rotation factor must be 0.2"
+assert len(augmenter.layers) == 2, "The model must have only 2 layers"
+
+print('\033[92mAll tests passed!')
