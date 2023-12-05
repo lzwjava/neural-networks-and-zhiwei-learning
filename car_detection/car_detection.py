ad82e51d56be141246c5f5c5d32bfb1b7fc6c1e3
lzwjava <lzwjava@gmail.com>
2023-12-06 07:15:08 +0800

    add

--- car_detection/car_detection.py
+++ car_detection/car_detection.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 import os
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
@@ -63,13 +64,13 @@ def iou(box1, box2):
     (box1_x1, box1_y1, box1_x2, box1_y2) = box1
     (box2_x1, box2_y1, box2_x2, box2_y2) = box2
 
-    xi1 = np.max(box1_x1, box2_x1)
-    yi1 = np.max(box1_y1, box2_y1)
-    xi2 = np.min(box1_x2, box2_x2)
-    yi2 = np.min(box1_y2, box2_y2)
+    xi1 = max(box1_x1, box2_x1)
+    yi1 = max(box1_y1, box2_y1)
+    xi2 = min(box1_x2, box2_x2)
+    yi2 = min(box1_y2, box2_y2)
     inter_width = xi2 - xi1
     inter_height = yi2 - yi1
-    inter_area = inter_width * inter_height
+    inter_area = max(inter_width, 0) * max(inter_height, 0)
 
     box1_area = (box1_x2 - box1_x1) * (box1_y2 - box1_y1)
     box2_area = (box2_x2 - box2_x1) * (box2_y2 - box2_y1)
@@ -102,5 +103,46 @@ box1 = (1, 1, 3, 3)
 box2 = (2, 3, 3, 4)
 print("iou for boxes that only touch at edges = " + str(iou(box1, box2)))
 assert iou(box1, box2) == 0, "Intersection at edges must be 0"
+
+print("\033[92m All tests passed!")
+
+
+def yolo_non_max_suppression(scores, boxes, classes, max_boxes=10, iou_threshold=0.5):
+    max_boxes_tensor = tf.Variable(max_boxes, dtype='int32')
+
+    nms_indices = None
+
+    scores = None
+    boxes = None
+    classes = None
+
+    return scores, boxes, classes
+
+
+tf.random.set_seed(10)
+scores = tf.random.normal([54, ], mean=1, stddev=4, seed=1)
+boxes = tf.random.normal([54, 4], mean=1, stddev=4, seed=1)
+classes = tf.random.normal([54, ], mean=1, stddev=4, seed=1)
+scores, boxes, classes = yolo_non_max_suppression(scores, boxes, classes)
+
+assert type(scores) == EagerTensor, "Use tensoflow functions"
+print("scores[2] = " + str(scores[2].numpy()))
+print("boxes[2] = " + str(boxes[2].numpy()))
+print("classes[2] = " + str(classes[2].numpy()))
+print("scores.shape = " + str(scores.numpy().shape))
+print("boxes.shape = " + str(boxes.numpy().shape))
+print("classes.shape = " + str(classes.numpy().shape))
+
+assert type(scores) == EagerTensor, "Use tensoflow functions"
+assert type(boxes) == EagerTensor, "Use tensoflow functions"
+assert type(classes) == EagerTensor, "Use tensoflow functions"
+
+assert scores.shape == (10,), "Wrong shape"
+assert boxes.shape == (10, 4), "Wrong shape"
+assert classes.shape == (10,), "Wrong shape"
+
+assert np.isclose(scores[2].numpy(), 8.147684), "Wrong value on scores"
+assert np.allclose(boxes[2].numpy(), [6.0797963, 3.743308, 1.3914018, -0.34089637]), "Wrong value on boxes"
+assert np.isclose(classes[2].numpy(), 1.7079165), "Wrong value on classes"
 
 print("\033[92m All tests passed!")
