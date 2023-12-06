12ef55f0075f415e0c5e1b8b325eafc22900f16d
lzwjava <lzwjava@gmail.com>
2023-12-06 10:41:14 +0800

    add

--- face_recognition/face_recognition.py
+++ face_recognition/face_recognition.py
@@ -19,11 +19,86 @@ import PIL
 
 from tensorflow.keras.models import model_from_json
 
-json_file = open('keras-facenet-h5/model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-model = model_from_json(loaded_model_json)
-model.load_weights('keras-facenet-h5/model.h5')
+tf.keras.__internal__.enable_unsafe_deserialization()
 
-print(model.inputs)
-print(model.outputs)
+
+# json_file = open('keras-facenet-h5/model.json', 'r')
+# loaded_model_json = json_file.read()
+# json_file.close()
+# model = model_from_json(loaded_model_json)
+# model.load_weights('keras-facenet-h5/model.h5')
+#
+# print(model.inputs)
+# print(model.outputs)
+
+
+# UNQ_C1(UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: triplet_loss
+
+def triplet_loss(y_true, y_pred, alpha=0.2):
+    """
+    Implementation of the triplet loss as defined by formula (3)
+
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor images, of shape (None, 128)
+            positive -- the encodings for the positive images, of shape (None, 128)
+            negative -- the encodings for the negative images, of shape (None, 128)
+
+    Returns:
+    loss -- real number, value of the loss
+    """
+
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+
+    ### START CODE HERE
+    # (≈ 4 lines)
+    # Step 1: Compute the (encoding) distance between the anchor and the positive
+    pos_dist = None
+    # Step 2: Compute the (encoding) distance between the anchor and the negative
+    neg_dist = None
+    # Step 3: subtract the two previous distances and add alpha.
+    basic_loss = None
+    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
+    loss = None
+    ### END CODE HERE
+
+    return loss
+
+
+### YOU CANNOT EDIT THIS CELL
+
+# BEGIN UNIT TEST
+tf.random.set_seed(1)
+y_true = (None, None, None)  # It is not used
+y_pred = (tf.keras.backend.random_normal([3, 128], mean=6, stddev=0.1, seed=1),
+          tf.keras.backend.random_normal([3, 128], mean=1, stddev=1, seed=1),
+          tf.keras.backend.random_normal([3, 128], mean=3, stddev=4, seed=1))
+loss = triplet_loss(y_true, y_pred)
+
+assert type(loss) == tf.python.framework.ops.EagerTensor, "Use tensorflow functions"
+print("loss = " + str(loss))
+
+y_pred_perfect = ([[1., 1.]], [[1., 1.]], [[1., 1., ]])
+loss = triplet_loss(y_true, y_pred_perfect, 5)
+assert loss == 5, "Wrong value. Did you add the alpha to basic_loss?"
+y_pred_perfect = ([[1., 1.]], [[1., 1.]], [[0., 0., ]])
+loss = triplet_loss(y_true, y_pred_perfect, 3)
+assert loss == 1., "Wrong value. Check that pos_dist = 0 and neg_dist = 2 in this example"
+y_pred_perfect = ([[1., 1.]], [[0., 0.]], [[1., 1., ]])
+loss = triplet_loss(y_true, y_pred_perfect, 0)
+assert loss == 2., "Wrong value. Check that pos_dist = 2 and neg_dist = 0 in this example"
+y_pred_perfect = ([[0., 0.]], [[0., 0.]], [[0., 0., ]])
+loss = triplet_loss(y_true, y_pred_perfect, -2)
+assert loss == 0, "Wrong value. Are you taking the maximum between basic_loss and 0?"
+y_pred_perfect = ([[1., 0.], [1., 0.]], [[1., 0.], [1., 0.]], [[0., 1.], [0., 1.]])
+loss = triplet_loss(y_true, y_pred_perfect, 3)
+assert loss == 2., "Wrong value. Are you applying tf.reduce_sum to get the loss?"
+y_pred_perfect = ([[1., 1.], [2., 0.]], [[0., 3.], [1., 1.]], [[1., 0.], [0., 1., ]])
+loss = triplet_loss(y_true, y_pred_perfect, 1)
+if (loss == 4.):
+    raise Exception('Perhaps you are not using axis=-1 in reduce_sum?')
+assert loss == 5, "Wrong value. Check your implementation"
+
+# END UNIT TEST
