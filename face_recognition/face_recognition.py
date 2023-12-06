db845ef398f3eb8f0e895aadb69a94e3c04796c7
lzwjava <lzwjava@gmail.com>
2023-12-06 10:08:05 +0800

    add

---
+++ face_recognition/face_recognition.py
@@ -0,0 +1,18 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import MaxPooling2D, AveragePooling2D
+from tensorflow.keras.layers import Concatenate
+from tensorflow.keras.layers import Lambda, Flatten, Dense
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import backend as K
+
+K.set_image_data_format('channels_last')
+import os
+import numpy as np
+from numpy import genfromtxt
+import pandas as pd
+import tensorflow as tf
+import PIL
