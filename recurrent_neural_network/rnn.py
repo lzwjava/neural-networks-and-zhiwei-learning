7718c533c4da7a56b3cd0b729a3c2cf78f21888d
lzwjava <lzwjava@gmail.com>
2023-12-07 07:12:20 +0800

    add

--- recurrent_neural_network/rnn.py
+++ recurrent_neural_network/rnn.py
@@ -36,3 +36,49 @@ print("yt_pred[1] =\n", yt_pred_tmp[1])
 print("yt_pred.shape = \n", yt_pred_tmp.shape)
 
 rnn_cell_forward_tests(rnn_cell_forward)
+
+
+def rnn_forward(x, a0, parameters):
+    caches = []
+
+    n_x, m, T_x = x.shape
+    n_y, n_a = parameters["Wya"].shape
+
+    a = None
+    y_pred = None
+
+    a_next = None
+
+    for t in range(T_x):
+        a_next, yt_pred, cache = None
+
+        a[:, :, t] = None
+
+        y_pred[:, :, t] = None
+
+        caches.append(None)
+
+    caches = (caches, x)
+
+    return a, y_pred, caches
+
+
+np.random.seed(1)
+x_tmp = np.random.randn(3, 10, 4)
+a0_tmp = np.random.randn(5, 10)
+parameters_tmp = {}
+parameters_tmp['Waa'] = np.random.randn(5, 5)
+parameters_tmp['Wax'] = np.random.randn(5, 3)
+parameters_tmp['Wya'] = np.random.randn(2, 5)
+parameters_tmp['ba'] = np.random.randn(5, 1)
+parameters_tmp['by'] = np.random.randn(2, 1)
+
+a_tmp, y_pred_tmp, caches_tmp = rnn_forward(x_tmp, a0_tmp, parameters_tmp)
+print("a[4][1] = \n", a_tmp[4][1])
+print("a.shape = \n", a_tmp.shape)
+print("y_pred[1][3] =\n", y_pred_tmp[1][3])
+print("y_pred.shape = \n", y_pred_tmp.shape)
+print("caches[1][1][3] =\n", caches_tmp[1][1][3])
+print("len(caches) = \n", len(caches_tmp))
+
+rnn_forward_test(rnn_forward)
