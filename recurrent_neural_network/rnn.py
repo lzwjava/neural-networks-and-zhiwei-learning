440b0379f6ee4938a71849a735a1c3ae569ffd48
lzwjava <lzwjava@gmail.com>
2023-12-08 01:12:56 +0800

    add

--- recurrent_neural_network/rnn.py
+++ recurrent_neural_network/rnn.py
@@ -44,19 +44,19 @@ def rnn_forward(x, a0, parameters):
     n_x, m, T_x = x.shape
     n_y, n_a = parameters["Wya"].shape
 
-    a = None
-    y_pred = None
+    a = np.zeros((n_a, m, T_x))
+    y_pred = np.zeros((n_y, m, T_x))
 
-    a_next = None
+    a_next = a0
 
     for t in range(T_x):
-        a_next, yt_pred, cache = None
+        a_next, yt_pred, cache = rnn_cell_forward(x[:, :, t], a_next, parameters)
 
-        a[:, :, t] = None
+        a[:, :, t] = a_next
 
-        y_pred[:, :, t] = None
+        y_pred[:, :, t] = yt_pred
 
-        caches.append(None)
+        caches.append(cache)
 
     caches = (caches, x)
 
@@ -82,3 +82,125 @@ print("caches[1][1][3] =\n", caches_tmp[1][1][3])
 print("len(caches) = \n", len(caches_tmp))
 
 rnn_forward_test(rnn_forward)
+
+
+def lstm_cell_forward(xt, a_prev, c_prev, parameters):
+    Wf = parameters["Wf"]
+    bf = parameters["bf"]
+    Wi = parameters["Wi"]
+    bi = parameters["bi"]
+    Wc = parameters["Wc"]
+    bc = parameters["bc"]
+    Wo = parameters["Wo"]
+    bo = parameters["bo"]
+    Wy = parameters["Wy"]
+    by = parameters["by"]
+
+    n_x, m = xt.shape
+    n_y, n_a = Wy.shape
+
+    concat = np.concatenate((a_prev, xt), axis=0)
+
+    ft = sigmoid(np.dot(Wf, concat) + bf)
+    it = sigmoid(np.dot(Wi, concat) + bi)
+    cct = np.tanh(np.dot(Wc, concat) + bc)
+    c_next = ft * c_prev + it * cct
+    ot = sigmoid(np.dot(Wo, concat) + bo)
+    a_next = ot * np.tanh(c_next)
+
+    yt_pred = softmax(np.dot(Wy, a_next) + by)
+
+    cache = (a_next, c_next, a_prev, c_prev, ft, it, cct, ot, xt, parameters)
+
+    return a_next, c_next, yt_pred, cache
+
+
+np.random.seed(1)
+xt_tmp = np.random.randn(3, 10)
+a_prev_tmp = np.random.randn(5, 10)
+c_prev_tmp = np.random.randn(5, 10)
+parameters_tmp = {}
+parameters_tmp['Wf'] = np.random.randn(5, 5 + 3)
+parameters_tmp['bf'] = np.random.randn(5, 1)
+parameters_tmp['Wi'] = np.random.randn(5, 5 + 3)
+parameters_tmp['bi'] = np.random.randn(5, 1)
+parameters_tmp['Wo'] = np.random.randn(5, 5 + 3)
+parameters_tmp['bo'] = np.random.randn(5, 1)
+parameters_tmp['Wc'] = np.random.randn(5, 5 + 3)
+parameters_tmp['bc'] = np.random.randn(5, 1)
+parameters_tmp['Wy'] = np.random.randn(2, 5)
+parameters_tmp['by'] = np.random.randn(2, 1)
+
+a_next_tmp, c_next_tmp, yt_tmp, cache_tmp = lstm_cell_forward(xt_tmp, a_prev_tmp, c_prev_tmp, parameters_tmp)
+
+print("a_next[4] = \n", a_next_tmp[4])
+print("a_next.shape = ", a_next_tmp.shape)
+print("c_next[2] = \n", c_next_tmp[2])
+print("c_next.shape = ", c_next_tmp.shape)
+print("yt[1] =", yt_tmp[1])
+print("yt.shape = ", yt_tmp.shape)
+print("cache[1][3] =\n", cache_tmp[1][3])
+print("len(cache) = ", len(cache_tmp))
+
+lstm_cell_forward_test(lstm_cell_forward)
+
+
+def lstm_forward(x, a0, parameters):
+    caches = []
+
+    Wy = parameters[
+        'Wy']
+
+    n_x, m, T_x = None
+    n_y, n_a = None
+
+    a = None
+    c = None
+    y = None
+
+    a_next = None
+    c_next = None
+
+    for t in range(T_x):
+        xt = x[:, :, t]
+
+        a_next, c_next, yt, cache = None
+
+        a[:, :, t] = None
+
+        c[:, :, t] = None
+
+        y[:, :, t] = None
+
+        None
+
+    caches = (caches, x)
+
+    return a, y, c, caches
+
+
+np.random.seed(1)
+x_tmp = np.random.randn(3, 10, 7)
+a0_tmp = np.random.randn(5, 10)
+parameters_tmp = {}
+parameters_tmp['Wf'] = np.random.randn(5, 5 + 3)
+parameters_tmp['bf'] = np.random.randn(5, 1)
+parameters_tmp['Wi'] = np.random.randn(5, 5 + 3)
+parameters_tmp['bi'] = np.random.randn(5, 1)
+parameters_tmp['Wo'] = np.random.randn(5, 5 + 3)
+parameters_tmp['bo'] = np.random.randn(5, 1)
+parameters_tmp['Wc'] = np.random.randn(5, 5 + 3)
+parameters_tmp['bc'] = np.random.randn(5, 1)
+parameters_tmp['Wy'] = np.random.randn(2, 5)
+parameters_tmp['by'] = np.random.randn(2, 1)
+
+a_tmp, y_tmp, c_tmp, caches_tmp = lstm_forward(x_tmp, a0_tmp, parameters_tmp)
+print("a[4][3][6] = ", a_tmp[4][3][6])
+print("a.shape = ", a_tmp.shape)
+print("y[1][4][3] =", y_tmp[1][4][3])
+print("y.shape = ", y_tmp.shape)
+print("caches[1][1][1] =\n", caches_tmp[1][1][1])
+print("c[1][2][1]", c_tmp[1][2][1])
+print("len(caches) = ", len(caches_tmp))
+
+lstm_forward_test(lstm_forward)
