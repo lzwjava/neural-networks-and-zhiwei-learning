1a5e61b779f49476aa50c398b4ca3c4cbee4d7e0
lzwjava <lzwjava@gmail.com>
2023-12-08 02:14:22 +0800

    add

--- recurrent_neural_network/rnn.py
+++ recurrent_neural_network/rnn.py
@@ -158,7 +158,7 @@ def lstm_forward(x, a0, parameters):
     y = np.zeros((n_y, m, T_x))
 
     a_next = a0
-    c_next = x[0]
+    c_next = np.zeros(a_next.shape)
 
     for t in range(T_x):
         xt = x[:, :, t]
@@ -203,3 +203,53 @@ print("c[1][2][1]", c_tmp[1][2][1])
 print("len(caches) = ", len(caches_tmp))
 
 lstm_forward_test(lstm_forward)
+
+
+def rnn_cell_backward(da_next, cache):
+    (a_next, a_prev, xt, parameters) = cache
+
+    Wax = parameters["Wax"]
+    Waa = parameters["Waa"]
+    Wya = parameters["Wya"]
+    ba = parameters["ba"]
+    by = parameters["by"]
+
+    dtanh = None
+
+    dxt = None
+    dWax = None
+
+    da_prev = None
+    dWaa = None
+
+    dba = None
+
+    gradients = {"dxt": dxt, "da_prev": da_prev, "dWax": dWax, "dWaa": dWaa, "dba": dba}
+
+    return gradients
+
+
+np.random.seed(1)
+xt_tmp = np.random.randn(3, 10)
+a_prev_tmp = np.random.randn(5, 10)
+parameters_tmp = {}
+parameters_tmp['Wax'] = np.random.randn(5, 3)
+parameters_tmp['Waa'] = np.random.randn(5, 5)
+parameters_tmp['Wya'] = np.random.randn(2, 5)
+parameters_tmp['ba'] = np.random.randn(5, 1)
+parameters_tmp['by'] = np.random.randn(2, 1)
+
+a_next_tmp, yt_tmp, cache_tmp = rnn_cell_forward(xt_tmp, a_prev_tmp, parameters_tmp)
+
+da_next_tmp = np.random.randn(5, 10)
+gradients_tmp = rnn_cell_backward(da_next_tmp, cache_tmp)
+print("gradients[\"dxt\"][1][2] =", gradients_tmp["dxt"][1][2])
+print("gradients[\"dxt\"].shape =", gradients_tmp["dxt"].shape)
+print("gradients[\"da_prev\"][2][3] =", gradients_tmp["da_prev"][2][3])
+print("gradients[\"da_prev\"].shape =", gradients_tmp["da_prev"].shape)
+print("gradients[\"dWax\"][3][1] =", gradients_tmp["dWax"][3][1])
+print("gradients[\"dWax\"].shape =", gradients_tmp["dWax"].shape)
+print("gradients[\"dWaa\"][1][2] =", gradients_tmp["dWaa"][1][2])
+print("gradients[\"dWaa\"].shape =", gradients_tmp["dWaa"].shape)
+print("gradients[\"dba\"][4] =", gradients_tmp["dba"][4])
+print("gradients[\"dba\"].shape =", gradients_tmp["dba"].shape)
