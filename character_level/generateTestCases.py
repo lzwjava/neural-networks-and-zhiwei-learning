18709c090830391f49321ec278056c2d1bb94c84
lzwjava <lzwjava@gmail.com>
2023-12-08 08:11:39 +0800

    add

--- character_level/generateTestCases.py
+++ character_level/generateTestCases.py
@@ -1,4 +1,3 @@
-# New Generate Test Cases 
 from solutions import *
 import numpy as np
 import math
@@ -6,7 +5,6 @@ import os, sys
 import copy
 from random import shuffle
 
-# import tensorflow as tf
 sys.path.append('../')
 sys.path.append('../../')
 
@@ -27,7 +25,6 @@ data_size, vocab_size = len(data), len(chars)
 char_to_ix = {ch: i for i, ch in enumerate(sorted(chars))}
 ix_to_char = {i: ch for i, ch in enumerate(sorted(chars))}
 
-# set the seed to be able to replicate the same results.
 np.random.seed(3)
 
 dWax = np.random.randn(5, 3) * 10
@@ -39,12 +36,11 @@ gradients = {"dWax": dWax, "dWaa": dWaa, "dWya": dWya, "db": db, "dby": dby}
 gradients1 = copy.deepcopy(gradients)
 gradients = clip(gradients, 10)
 
-# generating test cases for sampling function
 vocab_size = 27
 n = 23
 n_a = 50
 a0 = np.random.randn(n_a, 1) * 0.2
-i0 = 1  # first character is ix_to_char[i0]
+i0 = 1
 Wax = np.random.randn(n_a, vocab_size)
 Waa = np.random.randn(n_a, n_a)
 Wya = np.random.randn(vocab_size, n_a)
@@ -53,7 +49,6 @@ by = np.random.randn(vocab_size, 1)
 parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b, "by": by}
 indexes = sample(parameters, char_to_ix, 0)
 
-# # generating test cases for optimize function
 vocab_size = 27
 n_a = 50
 a_prev = np.random.randn(n_a, 1) * 0.2
@@ -68,9 +63,7 @@ X = [12, 3, 5, 11, 22, 3]
 Y = [4, 14, 11, 22, 25, 26]
 loss, g, a_last = optimize(X, Y, a_prev, parameters2, learning_rate=0.01)
 
-# generating the model. Killing the print statements.
 with stdout_redirector.stdout_redirected():
-    # generating the model
     with open("dinos.txt") as f:
         examples = f.readlines()
         np.random.seed(0)
--- character_level/utils.py
+++ character_level/utils.py
@@ -12,13 +12,13 @@ def smooth(loss, cur_loss):
 
 def print_sample(sample_ix, ix_to_char):
     txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-    txt = txt[0].upper() + txt[1:]  # capitalize first character 
+    txt = txt[0].upper() + txt[1:]
     print('%s' % (txt,), end='')
 
 
 def get_sample(sample_ix, ix_to_char):
     txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-    txt = txt[0].upper() + txt[1:]  # capitalize first character 
+    txt = txt[0].upper() + txt[1:]
     return txt
 
 
@@ -32,23 +32,12 @@ def softmax(x):
 
 
 def initialize_parameters(n_a, n_x, n_y):
-    """
-    Initialize parameters with small random values
-    
-    Returns:
-    parameters -- python dictionary containing:
-                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
-                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
-                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
-                        b --  Bias, numpy array of shape (n_a, 1)
-                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
-    """
     np.random.seed(1)
-    Wax = np.random.randn(n_a, n_x) * 0.01  # input to hidden
-    Waa = np.random.randn(n_a, n_a) * 0.01  # hidden to hidden
-    Wya = np.random.randn(n_y, n_a) * 0.01  # hidden to output
-    b = np.zeros((n_a, 1))  # hidden bias
-    by = np.zeros((n_y, 1))  # output bias
+    Wax = np.random.randn(n_a, n_x) * 0.01
+    Waa = np.random.randn(n_a, n_a) * 0.01
+    Wya = np.random.randn(n_y, n_a) * 0.01
+    b = np.zeros((n_a, 1))
+    by = np.zeros((n_y, 1))
 
     parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b, "by": by}
 
@@ -57,9 +46,9 @@ def initialize_parameters(n_a, n_x, n_y):
 
 def rnn_step_forward(parameters, a_prev, x):
     Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
-    a_next = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b)  # hidden state
+    a_next = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b)
     p_t = softmax(
-        np.dot(Wya, a_next) + by)  # unnormalized log probabilities for next chars # probabilities for next chars
+        np.dot(Wya, a_next) + by)
 
     return a_next, p_t
 
@@ -67,8 +56,8 @@ def rnn_step_forward(parameters, a_prev, x):
 def rnn_step_backward(dy, gradients, parameters, x, a, a_prev):
     gradients['dWya'] += np.dot(dy, a.T)
     gradients['dby'] += dy
-    da = np.dot(parameters['Wya'].T, dy) + gradients['da_next']  # backprop into h
-    daraw = (1 - a * a) * da  # backprop through tanh nonlinearity
+    da = np.dot(parameters['Wya'].T, dy) + gradients['da_next']
+    daraw = (1 - a * a) * da
     gradients['db'] += daraw
     gradients['dWax'] += np.dot(daraw, x.T)
     gradients['dWaa'] += np.dot(daraw, a_prev.T)
@@ -86,26 +75,20 @@ def update_parameters(parameters, gradients, lr):
 
 
 def rnn_forward(X, Y, a0, parameters, vocab_size=27):
-    # Initialize x, a and y_hat as empty dictionaries
     x, a, y_hat = {}, {}, {}
 
     a[-1] = np.copy(a0)
 
-    # initialize your loss to 0
     loss = 0
 
     for t in range(len(X)):
 
-        # Set x[t] to be the one-hot vector representation of the t'th character in X.
-        # if X[t] == None, we just have x[t]=0. This is used to set the input for the first timestep to the zero vector. 
         x[t] = np.zeros((vocab_size, 1))
         if (X[t] != None):
             x[t][X[t]] = 1
 
-        # Run one step forward of the RNN
         a[t], y_hat[t] = rnn_step_forward(parameters, a[t - 1], x[t])
 
-        # Update the loss by substracting the cross-entropy term of this time-step from it.
         loss -= np.log(y_hat[t][Y[t], 0])
 
     cache = (y_hat, a, x)
@@ -114,24 +97,18 @@ def rnn_forward(X, Y, a0, parameters, vocab_size=27):
 
 
 def rnn_backward(X, Y, parameters, cache):
-    # Initialize gradients as an empty dictionary
     gradients = {}
 
-    # Retrieve from cache and parameters
     (y_hat, a, x) = cache
     Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
 
-    # each one should be initialized to zeros of the same dimension as its corresponding parameter
     gradients['dWax'], gradients['dWaa'], gradients['dWya'] = np.zeros_like(Wax), np.zeros_like(Waa), np.zeros_like(Wya)
     gradients['db'], gradients['dby'] = np.zeros_like(b), np.zeros_like(by)
     gradients['da_next'] = np.zeros_like(a[0])
 
-    ### START CODE HERE ###
-    # Backpropagate through time
     for t in reversed(range(len(X))):
         dy = np.copy(y_hat[t])
         dy[Y[t]] -= 1
         gradients = rnn_step_backward(dy, gradients, parameters, x[t], a[t], a[t - 1])
-    ### END CODE HERE ###
 
     return gradients, a
