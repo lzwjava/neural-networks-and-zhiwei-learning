3dbc46e8d30fa66314ae8a383b94ee7f9c1eff2b
lzwjava <lzwjava@gmail.com>
2023-12-09 04:27:30 +0800

    add

--- word_vectors/word_vectors.py
+++ word_vectors/word_vectors.py
@@ -56,7 +56,7 @@ cosine_similarity_test(cosine_similarity)
 def complete_analogy(word_a, word_b, word_c, word_to_vec_map):
     word_a, word_b, word_c = word_a.lower(), word_b.lower(), word_c.lower()
 
-    e_a, e_b, e_c = None
+    e_a, e_b, e_c = word_to_vec_map[word_a], word_to_vec_map[word_b], word_to_vec_map[word_c]
 
     words = word_to_vec_map.keys()
     max_cosine_sim = -100
@@ -67,11 +67,11 @@ def complete_analogy(word_a, word_b, word_c, word_to_vec_map):
         if w == word_c:
             continue
 
-        cosine_sim = None
+        cosine_sim = cosine_similarity(e_b - e_a, word_to_vec_map[w] - e_c)
 
-        if None > None:
-            max_cosine_sim = None
-            best_word = None
+        if cosine_sim > max_cosine_sim:
+            max_cosine_sim = cosine_sim
+            best_word = w
 
     return best_word
 
@@ -110,3 +110,81 @@ def complete_analogy_test(target):
 
 
 complete_analogy_test(complete_analogy)
+
+triads_to_try = [('italy', 'italian', 'spain'), ('india', 'delhi', 'japan'), ('man', 'woman', 'boy'),
+                 ('small', 'smaller', 'large')]
+for triad in triads_to_try:
+    print('{} -> {} :: {} -> {}'.format(*triad, complete_analogy(*triad, word_to_vec_map)))
+
+g = word_to_vec_map['woman'] - word_to_vec_map['man']
+print(g)
+
+print('List of names and their similarities with constructed vector:')
+
+name_list = ['john', 'marie', 'sophie', 'ronaldo', 'priya', 'rahul', 'danielle', 'reza', 'katy', 'yasmin']
+
+for w in name_list:
+    print(w, cosine_similarity(word_to_vec_map[w], g))
+
+print('Other words and their similarities:')
+word_list = ['lipstick', 'guns', 'science', 'arts', 'literature', 'warrior', 'doctor', 'tree', 'receptionist',
+             'technology', 'fashion', 'teacher', 'engineer', 'pilot', 'computer', 'singer']
+for w in word_list:
+    print(w, cosine_similarity(word_to_vec_map[w], g))
+
+from tqdm import tqdm
+
+word_to_vec_map_unit_vectors = {
+    word: embedding / np.linalg.norm(embedding)
+    for word, embedding in tqdm(word_to_vec_map.items())
+}
+g_unit = word_to_vec_map_unit_vectors['woman'] - word_to_vec_map_unit_vectors['man']
+
+
+def neutralize(word, g, word_to_vec_map):
+    e = None
+
+    e_biascomponent = None
+
+    e_debiased = None
+
+    return e_debiased
+
+
+word = "receptionist"
+print("cosine similarity between " + word + " and g, before neutralizing: ",
+      cosine_similarity(word_to_vec_map[word], g))
+
+e_debiased = neutralize(word, g_unit, word_to_vec_map_unit_vectors)
+print("cosine similarity between " + word + " and g_unit, after neutralizing: ", cosine_similarity(e_debiased, g_unit))
+
+
+def equalize(pair, bias_axis, word_to_vec_map):
+    w1, w2 = None
+    e_w1, e_w2 = None
+
+    mu = None
+
+    mu_B = None
+    mu_orth = None
+
+    e_w1B = None
+    e_w2B = None
+
+    corrected_e_w1B = None
+    corrected_e_w2B = None
+
+    e1 = None
+    e2 = None
+
+    return e1, e2
+
+
+print("cosine similarities before equalizing:")
+print("cosine_similarity(word_to_vec_map[\"man\"], gender) = ", cosine_similarity(word_to_vec_map["man"], g))
+print("cosine_similarity(word_to_vec_map[\"woman\"], gender) = ", cosine_similarity(word_to_vec_map["woman"], g))
+print()
+e1, e2 = equalize(("man", "woman"), g_unit, word_to_vec_map_unit_vectors)
+print("cosine similarities after equalizing:")
+print("cosine_similarity(e1, gender) = ", cosine_similarity(e1, g_unit))
+print("cosine_similarity(e2, gender) = ", cosine_similarity(e2, g_unit))
