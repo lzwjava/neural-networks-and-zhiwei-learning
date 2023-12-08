35a7e3d1ce89c01c491351dffcc34a3170776018
lzwjava <lzwjava@gmail.com>
2023-12-09 04:33:00 +0800

    add

---
+++ emojify/emoji.py
@@ -0,0 +1,376 @@
+import numpy as np
+from emo_utils import *
+import emoji
+import matplotlib.pyplot as plt
+from test_utils import *
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+X_train, Y_train = read_csv('data/train_emoji.csv')
+X_test, Y_test = read_csv('data/tesss.csv')
+
+maxLen = len(max(X_train, key=lambda x: len(x.split())).split())
+
+for idx in range(10):
+    print(X_train[idx], label_to_emoji(Y_train[idx]))
+
+Y_oh_train = convert_to_one_hot(Y_train, C=5)
+Y_oh_test = convert_to_one_hot(Y_test, C=5)
+
+idx = 50
+print(f"Sentence '{X_train[idx]}' has label index {Y_train[idx]}, which is emoji {label_to_emoji(Y_train[idx])}", )
+print(f"Label index {Y_train[idx]} in one-hot encoding format is {Y_oh_train[idx]}")
+
+word_to_index, index_to_word, word_to_vec_map = read_glove_vecs('data/glove.6B.50d.txt')
+
+word = "cucumber"
+idx = 289846
+print("the index of", word, "in the vocabulary is", word_to_index[word])
+print("the", str(idx) + "th word in the vocabulary is", index_to_word[idx])
+
+
+def sentence_to_avg(sentence, word_to_vec_map):
+    any_word = list(word_to_vec_map.keys())[0]
+
+    words = None
+
+    avg = None
+
+    count = None
+
+    for w in None:
+
+        if w in None:
+            avg += None
+
+            count += 1
+
+    if count > 0:
+        avg = None
+
+    return avg
+
+
+avg = sentence_to_avg("Morrocan couscous is my favorite dish", word_to_vec_map)
+print("avg = \n", avg)
+
+
+def sentence_to_avg_test(target):
+    word_to_vec_map = {'a': [3, 3], 'synonym_of_a': [3, 3], 'a_nw': [2, 4], 'a_s': [3, 2],
+                       'c': [-2, 1], 'c_n': [-2, 2], 'c_ne': [-1, 2], 'c_e': [-1, 1], 'c_se': [-1, 0],
+                       'c_s': [-2, 0], 'c_sw': [-3, 0], 'c_w': [-3, 1], 'c_nw': [-3, 2]
+                       }
+
+    for key in word_to_vec_map.keys():
+        word_to_vec_map[key] = np.array(word_to_vec_map[key])
+
+    avg = target("a a_nw c_w a_s", word_to_vec_map)
+    assert tuple(avg.shape) == tuple(word_to_vec_map['a'].shape), "Check the shape of your avg array"
+    assert np.allclose(avg, [1.25, 2.5]), "Check that you are finding the 4 words"
+    avg = target("love a a_nw c_w a_s", word_to_vec_map)
+    assert np.allclose(avg, [1.25, 2.5]), "Divide by count, not len(words)"
+    avg = target("love", word_to_vec_map)
+    assert np.array_equal(avg, [0, 0]), "Average of no words must give an array of zeros"
+    avg = target("c_se foo a a_nw c_w a_s deeplearning c_nw", word_to_vec_map)
+    assert np.allclose(avg, [0.1666667, 2.0]), "Debug the last example"
+
+    print("\033[92mAll tests passed!")
+
+
+sentence_to_avg_test(sentence_to_avg)
+
+
+def model(X, Y, word_to_vec_map, learning_rate=0.01, num_iterations=400):
+    any_word = list(word_to_vec_map.keys())[0]
+
+    m = Y.shape[0]
+    n_y = len(np.unique(Y))
+    n_h = word_to_vec_map[any_word].shape[0]
+
+    W = np.random.randn(n_y, n_h) / np.sqrt(n_h)
+    b = np.zeros((n_y,))
+
+    Y_oh = convert_to_one_hot(Y, C=n_y)
+
+    for t in range(num_iterations):
+
+        cost = 0
+        dW = 0
+        db = 0
+
+        for i in range(m):
+            avg = None
+
+            z = None
+            a = None
+
+            cost += None
+
+            dz = a - Y_oh[i]
+            dW += np.dot(dz.reshape(n_y, 1), avg.reshape(1, n_h))
+            db += dz
+
+            W = W - learning_rate * dW
+            b = b - learning_rate * db
+
+        assert type(cost) == np.float64, "Incorrect implementation of cost"
+        assert cost.shape == (), "Incorrect implementation of cost"
+
+        if t % 100 == 0:
+            print("Epoch: " + str(t) + " --- cost = " + str(cost))
+            pred = predict(X, Y, W, b, word_to_vec_map)
+
+    return pred, W, b
+
+
+def model_test(target):
+    word_to_vec_map = {'a': [3, 3], 'synonym_of_a': [3, 3], 'a_nw': [2, 4], 'a_s': [3, 2], 'a_n': [3, 4],
+                       'c': [-2, 1], 'c_n': [-2, 2], 'c_ne': [-1, 2], 'c_e': [-1, 1], 'c_se': [-1, 0],
+                       'c_s': [-2, 0], 'c_sw': [-3, 0], 'c_w': [-3, 1], 'c_nw': [-3, 2]
+                       }
+
+    for key in word_to_vec_map.keys():
+        word_to_vec_map[key] = np.array(word_to_vec_map[key])
+
+    X = np.asarray(
+        ['a a_s synonym_of_a a_n c_sw', 'a a_s a_n c_sw', 'a_s  a a_n', 'synonym_of_a a a_s a_n c_sw', " a_s a_n",
+         " a a_s a_n c ", " a_n  a c c c_e",
+         'c c_nw c_n c c_ne', 'c_e c c_se c_s', 'c_nw c a_s c_e c_e', 'c_e a_nw c_sw', 'c_sw c c_ne c_ne'])
+
+    Y = np.asarray([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+
+    np.random.seed(10)
+    pred, W, b = model(X, Y, word_to_vec_map, 0.0025, 110)
+
+    assert W.shape == (2, 2), "W must be of shape 2 x 2"
+    assert np.allclose(pred.transpose(), Y), "Model must give a perfect accuracy"
+    assert np.allclose(b[0], -1 * b[1]), "b should be symmetric in this example"
+
+    print("\033[92mAll tests passed!")
+
+
+model_test(model)
+
+np.random.seed(1)
+pred, W, b = model(X_train, Y_train, word_to_vec_map)
+
+print("Training set:")
+pred_train = predict(X_train, Y_train, W, b, word_to_vec_map)
+print('Test set:')
+pred_test = predict(X_test, Y_test, W, b, word_to_vec_map)
+
+X_my_sentences = np.array(
+    ["i treasure you", "i love you", "funny lol", "lets play with a ball", "food is ready", "today is not good"])
+Y_my_labels = np.array([[0], [0], [2], [1], [4], [3]])
+
+pred = predict(X_my_sentences, Y_my_labels, W, b, word_to_vec_map)
+print_predictions(X_my_sentences, pred)
+
+print(Y_test.shape)
+print('           ' + label_to_emoji(0) + '    ' + label_to_emoji(1) + '    ' + label_to_emoji(
+    2) + '    ' + label_to_emoji(3) + '   ' + label_to_emoji(4))
+print(pd.crosstab(Y_test, pred_test.reshape(56, ), rownames=['Actual'], colnames=['Predicted'], margins=True))
+plot_confusion_matrix(Y_test, pred_test)
+
+import numpy as np
+import tensorflow
+
+np.random.seed(0)
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input, Dropout, LSTM, Activation
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.initializers import glorot_uniform
+
+np.random.seed(1)
+
+for idx, val in enumerate(["I", "like", "learning"]):
+    print(idx, val)
+
+
+def sentences_to_indices(X, word_to_index, max_len):
+    m = X.shape[0]
+
+    X_indices = None
+
+    for i in range(m):
+
+        sentence_words = None
+
+        j = None
+
+        for w in None:
+
+            if w in None:
+                X_indices[i, j] = None
+
+                j = None
+
+    return X_indices
+
+
+def sentences_to_indices_test(target):
+    word_to_index = {}
+    for idx, val in enumerate(["i", "like", "learning", "deep", "machine", "love", "smile", '´0.=']):
+        word_to_index[val] = idx + 1;
+
+    max_len = 4
+    sentences = np.array(["I like deep learning", "deep ´0.= love machine", "machine learning smile", "$"]);
+    indexes = target(sentences, word_to_index, max_len)
+    print(indexes)
+
+    assert type(indexes) == np.ndarray, "Wrong type. Use np arrays in the function"
+    assert indexes.shape == (sentences.shape[0], max_len), "Wrong shape of ouput matrix"
+    assert np.allclose(indexes, [[1, 2, 4, 3],
+                                 [4, 8, 6, 5],
+                                 [5, 3, 7, 0],
+                                 [0, 0, 0, 0]]), "Wrong values. Debug with the given examples"
+
+    print("\033[92mAll tests passed!")
+
+
+sentences_to_indices_test(sentences_to_indices)
+
+X1 = np.array(["funny lol", "lets play baseball", "food is ready for you"])
+X1_indices = sentences_to_indices(X1, word_to_index, max_len=5)
+print("X1 =", X1)
+print("X1_indices =\n", X1_indices)
+
+
+def pretrained_embedding_layer(word_to_vec_map, word_to_index):
+    vocab_size = len(word_to_index) + 1
+    any_word = list(word_to_vec_map.keys())[0]
+    emb_dim = word_to_vec_map[any_word].shape[0]
+
+    emb_matrix = None
+
+    for word, idx in word_to_index.items():
+        emb_matrix[idx, :] = None
+
+    embedding_layer = None
+
+    embedding_layer.build((None,))
+
+    embedding_layer.set_weights([emb_matrix])
+
+    return embedding_layer
+
+
+def pretrained_embedding_layer_test(target):
+    word_to_vec_map = {'a': [3, 3], 'synonym_of_a': [3, 3], 'a_nw': [2, 4], 'a_s': [3, 2], 'a_n': [3, 4],
+                       'c': [-2, 1], 'c_n': [-2, 2], 'c_ne': [-1, 2], 'c_e': [-1, 1], 'c_se': [-1, 0],
+                       'c_s': [-2, 0], 'c_sw': [-3, 0], 'c_w': [-3, 1], 'c_nw': [-3, 2]
+                       }
+
+    for key in word_to_vec_map.keys():
+        word_to_vec_map[key] = np.array(word_to_vec_map[key])
+
+    word_to_index = {}
+    for idx, val in enumerate(list(word_to_vec_map.keys())):
+        word_to_index[val] = idx;
+
+    np.random.seed(1)
+    embedding_layer = target(word_to_vec_map, word_to_index)
+
+    assert type(embedding_layer) == Embedding, "Wrong type"
+    assert embedding_layer.input_dim == len(list(word_to_vec_map.keys())) + 1, "Wrong input shape"
+    assert embedding_layer.output_dim == len(word_to_vec_map['a']), "Wrong output shape"
+    assert np.allclose(embedding_layer.get_weights(),
+                       [[[3, 3], [3, 3], [2, 4], [3, 2], [3, 4],
+                         [-2, 1], [-2, 2], [-1, 2], [-1, 1], [-1, 0],
+                         [-2, 0], [-3, 0], [-3, 1], [-3, 2], [0, 0]]]), "Wrong vaulues"
+    print("\033[92mAll tests passed!")
+
+
+pretrained_embedding_layer_test(pretrained_embedding_layer)
+
+embedding_layer = pretrained_embedding_layer(word_to_vec_map, word_to_index)
+print("weights[0][1][1] =", embedding_layer.get_weights()[0][1][1])
+print("Input_dim", embedding_layer.input_dim)
+print("Output_dim", embedding_layer.output_dim)
+
+
+def Emojify_V2(input_shape, word_to_vec_map, word_to_index):
+    sentence_indices = None
+
+    embedding_layer = None
+
+    embeddings = None
+
+    X = None
+
+    X = None
+
+    X = None
+
+    X = None
+
+    X = None
+
+    X = None
+
+    model = None
+
+    return model
+
+
+from tensorflow.python.keras.engine.functional import Functional
+
+
+def Emojify_V2_test(target):
+    word_to_vec_map = {'a': [3, 3], 'synonym_of_a': [3, 3], 'a_nw': [2, 4], 'a_s': [3, 2], 'a_n': [3, 4],
+                       'c': [-2, 1], 'c_n': [-2, 2], 'c_ne': [-1, 2], 'c_e': [-1, 1], 'c_se': [-1, 0],
+                       'c_s': [-2, 0], 'c_sw': [-3, 0], 'c_w': [-3, 1], 'c_nw': [-3, 2]
+                       }
+
+    for key in word_to_vec_map.keys():
+        word_to_vec_map[key] = np.array(word_to_vec_map[key])
+
+    word_to_index = {}
+    for idx, val in enumerate(list(word_to_vec_map.keys())):
+        word_to_index[val] = idx;
+
+    maxLen = 4
+    model = target((maxLen,), word_to_vec_map, word_to_index)
+
+    assert type(
+        model) == Functional, "Make sure you have correctly created Model instance which converts \"sentence_indices\" into \"X\""
+
+    expectedModel = [['InputLayer', [(None, 4)], 0], ['Embedding', (None, 4, 2), 30],
+                     ['LSTM', (None, 4, 128), 67072, (None, 4, 2), 'tanh', True], ['Dropout', (None, 4, 128), 0, 0.5],
+                     ['LSTM', (None, 128), 131584, (None, 4, 128), 'tanh', False], ['Dropout', (None, 128), 0, 0.5],
+                     ['Dense', (None, 5), 645, 'linear'], ['Activation', (None, 5), 0]]
+    comparator(summary(model), expectedModel)
+
+
+Emojify_V2_test(Emojify_V2)
+
+model = Emojify_V2((maxLen,), word_to_vec_map, word_to_index)
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+X_train_indices = sentences_to_indices(X_train, word_to_index, maxLen)
+Y_train_oh = convert_to_one_hot(Y_train, C=5)
+
+model.fit(X_train_indices, Y_train_oh, epochs=50, batch_size=32, shuffle=True)
+
+X_test_indices = sentences_to_indices(X_test, word_to_index, max_len=maxLen)
+Y_test_oh = convert_to_one_hot(Y_test, C=5)
+loss, acc = model.evaluate(X_test_indices, Y_test_oh)
+print()
+print("Test accuracy = ", acc)
+
+C = 5
+y_test_oh = np.eye(C)[Y_test.reshape(-1)]
+X_test_indices = sentences_to_indices(X_test, word_to_index, maxLen)
+pred = model.predict(X_test_indices)
+for i in range(len(X_test)):
+    x = X_test_indices
+    num = np.argmax(pred[i])
+    if (num != Y_test[i]):
+        print('Expected emoji:' + label_to_emoji(Y_test[i]) + ' prediction: ' + X_test[i] + label_to_emoji(num).strip())
+
+x_test = np.array(['I cannot play'])
+X_test_indices = sentences_to_indices(x_test, word_to_index, maxLen)
+print(x_test[0] + ' ' + label_to_emoji(np.argmax(model.predict(X_test_indices))))
