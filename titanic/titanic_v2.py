bf99595f6158ad0fa1a42d58f90e117a929a58b0
lzwjava <lzwjava@gmail.com>
2023-12-12 01:12:53 +0800

    add

--- titanic/titanic_v2.py
+++ titanic/titanic_v2.py
@@ -2,48 +2,96 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
-for dirname, _, filenames in os.walk('./'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
 
-train_data = pd.read_csv('./train.csv')
-test_data = pd.read_csv('./test.csv')
+class Net(nn.Module):
 
-print(train_data.head())
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(6, 30)
+        self.fc2 = nn.Linear(30, 1)
+        self.dropout = nn.Dropout(0.25)
 
-y = train_data['Survived']
-features = ['Age', 'Pclass', 'Sex', 'SibSp', 'Parch', 'Fare']
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = torch.tanh(x)
+        x = self.fc1(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
 
-train_data['Age'].fillna(0, inplace=True)
-test_data['Age'].fillna(0, inplace=True)
 
-train_data['Fare'].fillna(0, inplace=True)
-test_data['Fare'].fillna(0, inplace=True)
+def train(X, y):
+    model = Net()
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-X = pd.get_dummies(train_data[features])
-print(X)
+    X = torch.tensor(X.values, dtype=torch.float32)
+    y = torch.tensor(y.values, dtype=torch.float32).view(-1, 1)
 
-X_test = pd.get_dummies(test_data[features])
+    epochs = 1000
 
-model = RandomForestClassifier(n_estimators=100, max_depth=15, random_state=10)
-model.fit(X, y)
+    for epoch in range(epochs):
 
-predictions1 = model.predict(X)
+        optimizer.zero_grad()
 
-m = len(y)
+        outputs = model(X)
 
-count = 0
+        loss = criterion(outputs, y)
 
-for i in range(m):
-    if y[i] == predictions1[i]:
-        count += 1
+        loss.backward()
 
-print(f'rate={count / m}')
+        optimizer.step()
 
-predictions = model.predict(X_test)
+        if (epoch + 1) % 100 == 0:
+            print(f'Epoch [{epoch + 1}/{epochs}], Loss: {loss.item()}')
 
-output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
-output.to_csv('submission.csv', index=False)
 
-print('submitted')
+def main():
+    train_data = pd.read_csv('./train.csv')
+    test_data = pd.read_csv('./test.csv')
+
+    print(train_data.head())
+
+    y = train_data['Survived']
+    features = ['Age', 'Pclass', 'Sex', 'SibSp', 'Parch', 'Fare']
+
+    train_data['Age'].fillna(0, inplace=True)
+    test_data['Age'].fillna(0, inplace=True)
+
+    train_data['Fare'].fillna(0, inplace=True)
+    test_data['Fare'].fillna(0, inplace=True)
+
+    X = pd.get_dummies(train_data[features])
+    print(X)
+
+    X_test = pd.get_dummies(test_data[features])
+
+    model = RandomForestClassifier(n_estimators=100, max_depth=15, random_state=10)
+    model.fit(X, y)
+
+    predictions1 = model.predict(X)
+
+    m = len(y)
+
+    count = 0
+
+    for i in range(m):
+        if y[i] == predictions1[i]:
+            count += 1
+
+    print(f'rate={count / m}')
+
+    predictions = model.predict(X_test)
+
+    output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+    output.to_csv('submission.csv', index=False)
+
+    print('submitted')
+
+
+if __name__ == '__main__':
+    main()
