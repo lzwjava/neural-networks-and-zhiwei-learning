abd39da08c6c1ee2f084fab54b72ec42af3116ba
lzwjava <lzwjava@gmail.com>
2023-12-14 22:32:28 +0800

    add

--- playground-series-s3e26/s3e26_v2.py
+++ playground-series-s3e26/s3e26_v2.py
@@ -1,5 +1,13 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
+from sklearn.model_selection import train_test_split
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
 train_data = pd.read_csv('./train.csv')
 
@@ -9,34 +17,94 @@ print('Number of Training Examples = {}'.format(train_data.shape[0]))
 
 print(train_data.info())
 
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        hidden_unit = 10
+        self.fc1 = nn.Linear(8, hidden_unit)
+        self.fc2 = nn.Linear(hidden_unit, 1)
+        self.sigmoid = nn.Sigmoid()
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = torch.tanh(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return x
 
 
-def display_missing(df):
-    for col in df.columns.tolist():
-        print('{} column missing values: {}'.format(col, df[col].isnull().sum()))
-    print('\n')
+def train(model: Net, optimizer: optim.Adam, train_loader: DataLoader):
+    model.train()
+
+    criterion = nn.MSELoss()
+
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+
+        outputs = model(data)
+
+        loss = criterion(outputs, target)
+
+        loss.backward()
+
+        optimizer.step()
 
 
-display_missing(train_data)
+def test(model: Net, test_loader: DataLoader):
+    model.eval()
 
-# df_all_corr = df.corr().abs().unstack().sort_values(kind="quicksort", ascending=False).reset_index()
-# df_all_corr.rename(columns={"level_0": "Feature 1", "level_1": "Feature 2", 0: 'Correlation Coefficient'}, inplace=True)
-# df_all_corr[df_all_corr['Feature 1'] == 'Age']
-# print(df_all_corr)
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+
+            output_tensor = torch.where(output >= 0.5, torch.tensor(1.0), torch.tensor(0.0))
+
+            correct += output_tensor.eq(target.view_as(output_tensor)).sum().item()
+            total += len(data)
+
+    print(f'test correct rate: {correct / total}')
+
 
 gb = train_data.groupby('Status')
 print(gb)
 
-# want to see the distribution of the Status column
 print(gb.size())
 
-# and calculate the mean of each column for each Status
 print(gb.mean())
 
+
+def map_bool_values(value):
+    return 1 if value == 'Y' else 0
+
+
+def preprocess_data(data: pd.DataFrame):
+    status_mapping = {'C': 0, 'CL': 1, '1': 2}
+    data['Status'] = data['Status'].map(status_mapping)
+
+    sex_mapping = {'M': 0, 'F': 1}
+    data['Sex'] = data['Sex'].map(sex_mapping)
+
+    bool_items = ['Ascites', 'Hepatomegaly', 'Spiders', 'Edema']
+
+    for column in bool_items:
+        data[column] = data[column].map(map_bool_values)
+
+    return data
+
+
+train_data = preprocess_data(train_data)
+
 y = train_data['Status']
-features = ['N_Days', 'Copper', 'Alk_Phos', 'SGOT', 'Tryglicerides']
+
+features = ['N_Days', 'Age', 'Sex', 'Ascites', 'Hepatomegaly', 'Spiders', 'Edema',
+            'Bilirubin', 'Cholesterol', 'Albumin', 'Copper', 'Alk_Phos', 'SGOT',
+            'Tryglicerides', 'Platelets', 'Prothrombin', 'Stage']
 
 X = pd.get_dummies(train_data[features])
 
@@ -50,6 +118,28 @@ m = len(y)
 count = sum(1 for i in range(m) if y[i] == predictions1[i])
 
 print(f'rate={count / m}')
+
+X = torch.tensor(X.values, dtype=torch.float32)
+y = torch.tensor(y.values, dtype=torch.float32).view(-1, 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.02, random_state=42)
+
+batch_size = 30
+train_dataset = TensorDataset(X_train, y_train)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+model = Net()
+
+epochs = 100
+
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+test_dataset = TensorDataset(X_test, y_test)
+test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+for i in range(epochs):
+    train(model, optimizer, train_loader)
+    test(model, test_loader)
 
 test_data = pd.read_csv('./test.csv')
 X_test = pd.get_dummies(test_data[features])
