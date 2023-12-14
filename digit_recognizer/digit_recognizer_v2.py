f64883bb1f53283a28bee7f3630fd241faf5dae8
lzwjava <lzwjava@gmail.com>
2023-12-14 19:44:15 +0800

    add

--- digit_recognizer/digit_recognizer_v2.py
+++ digit_recognizer/digit_recognizer_v2.py
@@ -20,7 +20,7 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
         self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(0.25)
 
     def forward(self, x):
         x = self.conv1(x)
