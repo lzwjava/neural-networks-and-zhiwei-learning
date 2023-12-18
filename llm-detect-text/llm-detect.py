e4095e174568b3e956755efa4aeb02a1b1ffaec5
lzwjava <lzwjava@gmail.com>
2023-12-18 16:31:15 +0800

    add

--- llm-detect-text/llm-detect.py
+++ llm-detect-text/llm-detect.py
@@ -7,4 +7,5 @@ train_data = pd.read_csv('train_essays.csv')
 test_data = pd.read_csv('test_essays.csv')
 train_prompts = pd.read_csv('train_prompts.csv')
 
-print(train_data.head())
+print(train_data.head(10))
+print(test_data.head(10))
