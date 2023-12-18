d8a53cddfb4b7c6a8bec9cf850e0c13d4b8e9d3f
lzwjava <lzwjava@gmail.com>
2023-12-18 16:18:22 +0800

    add

--- llm-detect-text/llm-detect.py
+++ llm-detect-text/llm-detect.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
 train_data = pd.read_csv('train_essays.csv')
 test_data = pd.read_csv('test_essays.csv')
 train_prompts = pd.read_csv('train_prompts.csv')
