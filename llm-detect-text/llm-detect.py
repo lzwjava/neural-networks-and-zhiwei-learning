179b681583fcfdb715d8fda12e6d295cc0c4f679
lzwjava <lzwjava@gmail.com>
2023-12-20 09:14:50 +0800

    add

--- llm-detect-text/llm-detect.py
+++ llm-detect-text/llm-detect.py
@@ -4,10 +4,22 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 
-train_data = pd.read_csv('train_essays.csv')
-test_data = pd.read_csv('test_essays.csv')
-train_prompts = pd.read_csv('train_prompts.csv')
+dir_name = './'
+dir_name = '/kaggle/input/'
+
+train_data = pd.read_csv(dir_name + 'train_essays.csv')
+test_data = pd.read_csv(dir_name + 'test_essays.csv')
+train_prompts = pd.read_csv(dir_name + 'train_prompts.csv')
 
 print(train_data.head())
 print(test_data.head())
 print(train_prompts.head())
+
+ids = test_data['id'].tolist()
+
+n = len(ids)
+
+generated = [0.5] * n
+
+output = pd.DataFrame({'id': ids, 'generated': generated})
+output.to_csv('submission.csv', index=False)
